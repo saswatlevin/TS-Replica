90ff4e5646194427233949d0508756e2a53a6298
Saswat Levin <saswatlevin@gmail.com>
2025-05-28 23:51:49 +0300

    Added route for updating user password

--- Website/src/Middleware/Routes/userRoutes.js
+++ Website/src/Middleware/Routes/userRoutes.js
@@ -4,5 +4,6 @@ const userControllers = require('../Controllers/userControllers');
 
 router.route('/registeruser').post(userControllers.registerUser);
 router.route('/updateuser').put(userControllers.updateUser);
+router.route('/updateuserpassword').put(userControllers.updateUserPassword);
 
 module.exports = router;

56d1b44e88eeca30d8c18180ff7e764318e7530a
Saswat Levin <saswatlevin@gmail.com>
2025-05-28 19:37:09 +0300

    Renamed createUser to registerUser and added updateUser. Both working well

--- Website/src/Middleware/Controllers/userControllers.js
+++ Website/src/Middleware/Controllers/userControllers.js
@@ -3,12 +3,14 @@ const User = require('../Models/User');
 var passwordValidator = require('password-validator');
 const passwordSchema = require('../Security/PasswordSchemas/passwordSchema');
 const argon2 = require('argon2');
+const listOfKeys = require('../listOfKeys');
+const checkForbiddenKeys = require('../checkForbiddenKeys');
+const filterKeys = require('../filterKeys');
+const pruneObject = require('../pruneObject');
 
-// const userObject = require('../preComputedObjects');
-// const createDocument = require('../createDocument');
-// const mongoose = require('mongoose');
 
-const createUser = async (req, res, next) => {
+// CREATES A NEW USER
+const registerUser = async (req, res, next) => {
     // All data is provided in the request body.
     // Since the user document (request body) is manually generated, 
     // so we don't need to use createDocument yet.
@@ -36,10 +38,59 @@ const createUser = async (req, res, next) => {
     // Create the new User in the database
     const result = await User.create(requestBodyObjectCopy);
 
+    console.log("User created successfully");
+
     res.status(201).json(result);
 
 }
 
+// UPDATES ANY FIELD OF THE USER DOCUMENT EXCEPT ShippingAddresses, CartItems, docType, password AND user_id
+const updateUser = async(req, res, next) => {
+    
+    const requestBodyCopy = JSON.parse(JSON.stringify(req.body)); 
+    
+    const requestBodyKeys = Object.keys(requestBodyCopy);
+    
+    console.log("requestBodyKeys ", requestBodyKeys);
+    
+    var filter = {};
+
+    const forbiddenKey = checkForbiddenKeys("updateUser", requestBodyKeys);
+    
+    if (forbiddenKey) {
+        res.status(400).json("updateUser can't update the " + forbiddenKey + " field.");
+        return;
+    }
+
+    var filterKey = filterKeys.checkFilter("updateUser", requestBodyKeys);
+    
+
+    if (!filterKey) {
+        res.status(400).json("The updateUser API cannot find the requested User document to be updated using the " + filterKey + " field. Please provide the email or user_id fields' values.");
+        return;
+    }
+   
+    const updateObject = pruneObject(requestBodyCopy, [filterKey]);     
+    
+    filter[filterKey] = requestBodyCopy[filterKey];
+    console.log("requestBodyCopy ", requestBodyCopy);
+    console.log("filterKey ", filterKey);
+    console.log("updateObject ", updateObject);
+    console.log("filter ", filter);
+    
+
+    const updateResult = await User.findOneAndUpdate(filter, updateObject, {new: true}, {runValidators: true});
+
+    // console.log("updateResult ", updateResult);
+
+    res.status(201).json(updateResult);
+
+    console.log("After update operation");
+    
+
+}
+
 module.exports = { 
-    createUser
+    registerUser,
+    updateUser
 };

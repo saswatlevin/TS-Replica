a70c309fdbdef528f0511bb9f5b01603bad4274b
Saswat Levin <saswatlevin@gmail.com>
2025-06-27 03:07:39 +0300

    Added request validation middleware to all user routes

--- Website/src/Middleware/Routes/userRoutes.js
+++ Website/src/Middleware/Routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const userControllers = require('../Controllers/userControllers');
+const requestValidator = require('../Validators/requestValidator');
+const userSchemas = require('../Schemas/userSchemas');
 
-router.route('/registeruser').post(userControllers.registerUser);
-router.route('/updateuser').put(userControllers.updateUser);
-router.route('/updateuserpassword').put(userControllers.updateUserPassword);
+router.route('/registeruser').post(requestValidator(userSchemas.userRequestSchema), userControllers.registerUser);
+router.route('/updateuser').put(requestValidator(userSchemas.userUpdateSchema), userControllers.updateUser);
+router.route('/updateuserpassword').put(requestValidator(userSchemas.userPasswordUpdateSchema), userControllers.updateUserPassword);
+router.route('/getuserbyid').get(requestValidator(userSchemas.getUserByIdSchema), userControllers.getUserById);
+router.route('/searchusersbyname').get(requestValidator(userSchemas.getUsersByNameSchema), userControllers.searchUsersByName);
+//router.route('/objectidtest').get(userControllers.objectIdTest);
 
 module.exports = router;

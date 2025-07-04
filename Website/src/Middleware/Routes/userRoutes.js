01eeb11fa0178054914df5d95ed78d71d82db0b7
Saswat Levin <saswatlevin@gmail.com>
2025-07-04 15:04:50 +0300

    Added requestValidator with respective schema to all the routes in userRoutes.js

--- Website/src/Middleware/Routes/userRoutes.js
+++ Website/src/Middleware/Routes/userRoutes.js
@@ -5,10 +5,10 @@ const requestValidator = require('../Validators/requestValidator');
 const userSchemas = require('../Schemas/userSchemas');
 
 router.route('/registeruser').post(requestValidator(userSchemas.userRequestSchema), userControllers.registerUser);
-router.route('/updateuser').put(requestValidator(userSchemas.userUpdateSchema), userControllers.updateUser);
-router.route('/updateuserpassword').put(requestValidator(userSchemas.userPasswordUpdateSchema), userControllers.updateUserPassword);
+router.route('/updateuser').put(requestValidator(userSchemas.updateUserSchema), userControllers.updateUser);
+router.route('/updateuserpassword').put(requestValidator(userSchemas.updateUserPasswordSchema), userControllers.updateUserPassword);
 router.route('/getuserbyid').get(requestValidator(userSchemas.getUserByIdSchema), userControllers.getUserById);
-router.route('/searchusersbyname').get(requestValidator(userSchemas.getUsersByNameSchema), userControllers.searchUsersByName);
+router.route('/getusersbyname').get(requestValidator(userSchemas.getUsersByNameSchema), userControllers.searchUsersByName);
 //router.route('/objectidtest').get(userControllers.objectIdTest);
 
 module.exports = router;

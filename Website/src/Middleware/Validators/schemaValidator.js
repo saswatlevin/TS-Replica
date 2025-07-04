0f934e0951e180e2d3ec00e1885fe6ae9db2dc5b
Saswat Levin <saswatlevin@gmail.com>
2025-07-04 14:35:36 +0300

    Commented out console.log displaying object in schemaValidator.js

--- Website/src/Middleware/Validators/schemaValidator.js
+++ Website/src/Middleware/Validators/schemaValidator.js
@@ -2,6 +2,7 @@ const z = require('zod');
 
 const schemaValidator = (schema, object) => {
     console.log("In schemaValidator");
+    // console.log("object ", object);
     const schemaValidationResult = schema.parse(object);
     //console.log("schemaValidationResult ", schemaValidationResult);
     return schemaValidationResult;

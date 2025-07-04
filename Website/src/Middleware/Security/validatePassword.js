0f7e7b2ef3abcfb3699e3aebaa333b3d36c4dce7
Saswat Levin <saswatlevin@gmail.com>
2025-07-04 15:26:55 +0300

    Removed Security folder

--- Website/src/Middleware/Security/validatePassword.js
+++
@@ -1,9 +0,0 @@
-var passwordValidator = require('password-validator');
-const passwordSchema = require('./PasswordSchemas/passwordSchema');
-
-const validatePassword = (password) => {
-    const result = passwordSchema.validate(password);
-    return result;
-}
-
-module.exports = validatePassword;

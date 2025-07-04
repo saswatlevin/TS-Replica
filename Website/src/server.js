5c2a349da7572cf5e2133dc30eedcd0fbfd89eba
Saswat Levin <saswatlevin@gmail.com>
2025-07-04 14:42:28 +0300

    Called responseValidator in server.js

--- Website/src/server.js
+++ Website/src/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const connectToDB  = require('./Middleware/connectToDB');
+const responseValidator = require('./Middleware/Validators/responseValidator');
 require('dotenv').config();
+//const shippingAddressSchema = require('./Middleware/Schemas/shippingAddressSchemas');
+//const productSchema = require('./Middleware/Schemas/productSchemas');
+//const userSchemas = require('./Middleware/Schemas/userSchemas');
+//const orderSchemas = require('./Middleware/Schemas/orderSchemas');
+//const reviewSchemas = require('./Middleware/Schemas/reviewSchemas');
 
-const app = express();
 
 connectToDB();
 
 // Middleware to handle JSON request body.
 app.use(express.json());
+
+app.use(responseValidator);
 
 // Routes
 app.use('/', require('./Middleware/Routes/defaultRoute'));

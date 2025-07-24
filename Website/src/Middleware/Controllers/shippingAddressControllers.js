59e19aca7830eb51a4d9c7783450ea54217e2769
Saswat Levin <saswatlevin@gmail.com>
2025-07-24 23:29:37 +0300

    Added async error handler and resource not found error

--- Website/src/Middleware/Controllers/shippingAddressControllers.js
+++ Website/src/Middleware/Controllers/shippingAddressControllers.js
@@ -1,42 +1,37 @@
+const asyncErrorHandler = require('../ErrorHandlers/asyncErrorHandler');
 const { checkUserExists } = require('../Controllers/SupportFunctions/shippingAddressSupportFunctions');
 const mongoose = require('mongoose');
 const User = require('../Models/User');
 const { ValidationError } = mongoose.Error;
 const { ZodError } = require('zod');
+const ResourceNotFoundError = require('../OperationalErrors/ResourceNotFoundError');
 
-const createShippingAddress = async(req, res, next) => {
+const createShippingAddress =  async(req, res, next) => {
     console.log("In createShippingAddress");
 
     // Test whether value matches expected value
-    const shippingAddress = req?.body;
+    const shipping_address = req?.body;
     
     // Test return value
-    const userExists = await checkUserExists(req);
+    const user_exists = await checkUserExists(req);
 
-    const userID = req?.params?.userID;
+    console.log("user_exists ", user_exists);
 
-    if(userExists === false) {
-        res.status(400).json("Could not create shipping address as user could not be found.");
+    const user_id = req?.params?.user_id;
+
+    //console.log("user_id ", user_id);
+
+    //console.log("shipping_address ", shipping_address);
+
+    if(user_exists === false) {
+        const error = new ResourceNotFoundError(`Could not create the shipping address since the user with user_id ${user_id} does not exist.`);
+        throw error;
     }
     
     else {
-        try {
-            const result = await User.findOneAndUpdate({ user_id: userID }, { $push: { ShippingAddresses: shippingAddress } }, { new: true }); // return the updated document;
-            res.status(201).json(result);
-        }
-
-        catch(error) {
-            
-            if (error instanceof ZodError || error instanceof ValidationError) {
-                res.status(400).json(error);
-            }
-            
-            else {
-                console.log(error);
-                res.status(500).json(error);
-            }
-           
-        }
+        const result = await User.findOneAndUpdate({ user_id: user_id }, { $push: { ShippingAddresses: shipping_address } }, { new: true }); // return the updated document;
+        console.log("Shipping address created successfully, result ", result);
+        res.status(201).json(result);
     }
     
 };

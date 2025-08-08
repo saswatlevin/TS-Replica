68161e981d8f35db15ead53dee19ee372451b0ac
Saswat Levin <saswatlevin@gmail.com>
2025-08-09 00:30:36 +0300

    Added routes for deleteShippingAddress controller

--- Website/src/Middleware/Routes/shippingAddressRoutes.js
+++ Website/src/Middleware/Routes/shippingAddressRoutes.js
@@ -5,12 +5,14 @@ const requestValidator = require('../Validators/requestValidator');
 const shippingAddressSchemas = require('../Schemas/shippingAddressSchemas');
 const { xss } = require('express-xss-sanitizer');
 
-router.route('/createshippingaddress/:user_id').post(requestValidator(shippingAddressSchemas.shippingAddressRequestSchema), xss(), shippingAddressControllers.createShippingAddress);
+router.route('/createshippingaddress/:user_id').post(requestValidator(shippingAddressSchemas.createShippingAddressRequestSchema), xss(), shippingAddressControllers.createShippingAddress);
 
 router.route('/updateshippingaddress/:user_id/:shipping_address_id').patch(requestValidator(shippingAddressSchemas.updateShippingAddressRequestSchema), xss(), shippingAddressControllers.updateShippingAddress);
 
 router.route('/getshippingaddressbyid/:user_id').get(requestValidator(shippingAddressSchemas.getShippingAddressByIdRequestSchema), xss(), shippingAddressControllers.getShippingAddressById);
 
 router.route('/searchshippingaddress/:user_id').get(requestValidator(shippingAddressSchemas.searchShippingAddressRequestSchema), xss(), shippingAddressControllers.searchShippingAddress);
+
+router.route('/deleteshippingaddressbyid/:user_id').delete(requestValidator(shippingAddressSchemas.getShippingAddressByIdRequestSchema), xss(), shippingAddressControllers.deleteShippingAddressById);
 
 module.exports = router;

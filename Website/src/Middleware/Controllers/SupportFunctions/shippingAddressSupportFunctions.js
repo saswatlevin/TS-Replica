bd5361a8408773a648ae8aae9b925009d1da7675
Saswat Levin <saswatlevin@gmail.com>
2025-08-09 00:27:51 +0300

    Added a try and catch block to every shipping address support function. Also introduced the checkIsEmptObject function

--- Website/src/Middleware/Controllers/SupportFunctions/shippingAddressSupportFunctions.js
+++ Website/src/Middleware/Controllers/SupportFunctions/shippingAddressSupportFunctions.js
@@ -2,33 +2,25 @@ const asyncErrorHandler = require('../../ErrorHandlers/asyncErrorHandler');
 const User = require('../../Models/User');
 const _ = require('lodash');
 
-const checkUserExists = asyncErrorHandler( async(req, res, next) => {
+const checkUserExists = async(req, res, next) => {
     console.log("In checkUserExists");
+    
+    try {
+
     
     // So that we don't have an undefined user_id
     const user_id = req.params.user_id;
 
+    //console.log("user_id ", user_id);
+
     const user_query = {user_id: user_id};
+
+    //console.log("user_query ", user_query);
 
     // Check if findOne is called with the correct input and return value is as expected.
     const result = await User.findOne(user_query);
 
-    if (result === null) {
-        return false;
-    }
-
-    else {
-        return true;
-    }
-});
-
-const checkShippingAddressExists = asyncErrorHandler(async(req, res, next) => {
-    // So that we don't have an undefined shipping_address_id
-    const shipping_address_id = req.params.shipping_address_id;
-
-    const shipping_address_query = {"ShippingAddresses.shipping_address_id": shipping_address_id};
-
-    const result = User.findOne(shipping_address_query);
+    //console.log("result ", result);
 
     if (result === null) {
         return false;
@@ -37,13 +29,59 @@ const checkShippingAddressExists = asyncErrorHandler(async(req, res, next) => {
     else {
         return true;
     }
+}
 
-});
+catch(error) {
+    console.log("Error in checkUserExists ", error);
+    return error;
+}
+
+};
+
+const checkShippingAddressExists = async(req, res, next) => {
+    console.log("In checkShippingAddressExists");
+
+    try {
+        
+        // So that we don't have an undefined shipping_address_id
+        const shipping_address_id = req.params.shipping_address_id || req.body.shipping_address_id || undefined;
+
+        const shipping_address_query = {"ShippingAddresses.shipping_address_id": shipping_address_id};
+
+        const result = User.findOne(shipping_address_query);
+
+        if (result === null) {
+            return false;
+        }
+
+        else {
+            return true;
+        }
+    
+
+    }
+
+    catch (error) {
+        console.log("Error in checkShippingAddressExists ", error);
+        return error;
+    }
+
+    
+
+};
 
 const checkIsEmptyObject = (object) => {
     console.log("In checkIsEmptyObject");
-    const result = _.isEmpty(object);
-    return result;
+    
+    try {
+        const result = _.isEmpty(object);
+        return result;
+    }
+
+    catch(error) {
+        console.log("Error in checkIsEmptyObject ", error);
+    }
+   
 }
 
 module.exports = {

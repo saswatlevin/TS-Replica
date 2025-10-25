9829f557c28217a9ca31162907d14328f3cb22c8
Saswat Levin <saswatlevin@gmail.com>
2025-10-25 11:05:53 +0300

    Corrected a spelling in cartItemValidators.js

--- Website/src/Middleware/Validators/CustomValidators/cartItemValidators.js
+++ Website/src/Middleware/Validators/CustomValidators/cartItemValidators.js
@@ -3,8 +3,7 @@ const z = require('zod');
 
 const zodIscartItemId = z.string("The cart_item_id field must be a string. It is a required field.").length(12, {message: "The cart_item_id must be 12 characters long."}).regex(customValidators.twelveCharacterRegex, {message: "The cart_item_id can only contain lowercase letters and numbers."}).transform((val) => JSON.parse(val));
 
-
-const zodIsCartItemName = z.string("The cart_item_name field must be a string.").min(3, {message: "The cart_item_name field is a required field."}).max(100, {message: "The cart_item_name field has a maximum permitted length of 100 characters."}).regex(customValidators.productNameRegex, {message: "The cart_item_name can contain only small leters, capital letters, accented letters, digits, dashes and spaces."}).transform((val) => JSON.parse(val));
+const zodIsCartItemName = z.string("The cart_item_name field must be a string.").min(3, {message: "The cart_item_name field is a required field."}).max(100, {message: "The cart_item_name field has a maximum permitted length of 100 characters."}).regex(customValidators.productNameRegex, {message: "The cart_item_name can contain only small letters, capital letters, accented letters, digits, dashes and spaces."}).transform((val) => JSON.parse(val));
     
 const zodIsCartItemImageURI = z.string("The cart_item_image_uri must be a string. It is a required field.").max(256).regex(customValidators.windowsFilePathRegex, {message: "The cart_item_image_uri must be a valid Windows file path. Maximum permitted length is 256 characters."}).transform((val) => JSON.parse(val));
  

a1ce20a3f0f088af1d620622d0a13eb14ee2d8f2
Saswat K. Levin <saswatlevin@gmail.com>
2025-10-26 13:16:33 +0300

    Removed the user_id field from userUpdateSchema

--- Website/src/Middleware/Schemas/userSchemas.js
+++ Website/src/Middleware/Schemas/userSchemas.js
@@ -34,35 +34,7 @@ const userRequestSchema = z.object({
 
 });
 
-const userUpdateSchema = z.object({
-    user_id: z.string("The user_id field must be a string. It is a required field.").length(12, {message: "The user_id must be 12 characters long."}).regex(customValidators.twelveCharacterRegex, {message: "The user_id can only contain lowercase letters and numbers."}),
-
-    email: customValidators.zodIsEmail.optional(),
-    
-    phone_number: customValidators.zodIsMobilePhone.optional(),
-    
-    first_name: z.string("The first_name field must be a string.").min(1, {message: "The first_name field is a required field."}).max(100, {message: "The first_name field has a maximum permitted length of 100 characters."}).regex(customValidators.nameRegex, {message: "The first_name field can only contain uupercase letters, lowercase letters, hyphens and single-quotes."}).optional(),
-    
-    last_name: z.string("The last_name field must be a string.").min(1, {message: "The last_name field is a required field."}).max(100, {message: "The last_name field has a maximum permitted length of 100 characters."}).regex(customValidators.nameRegex, {message: "The last_name field can only contain uppercase letters, lowercase letters, hyphens and single-quotes."}).optional(),
-
-    user_role: z.enum(["user", "admin"], {message: "The user_role field is a required field. It takes 1 of the following values: user, admin."}).optional(),
-    
-    upper_size_number: customValidators.zodIsUpperSizeNumber.optional(),
-    
-    upper_size_letter: z.enum(customValidators.sizeLetterArray, {message: "The upper_size_letter field is a required field. It must be one of the following values: XXL, XL, L, M, S, XS."}).optional(),
-    
-    lower_size_number: customValidators.zodIsLowerSizeNumber.optional(),
-    
-    others_size_letter: z.enum(customValidators.sizeLetterArray, {message: "The others_size_letter field is a required field. It must be one of the following values: XXL, XL, L, M, S, XS."}).optional(),
-    
-    email_comms_type: z.enum(["I want all emails", "One weekly recap", "Stock notifications only", "Never / Unsubscribe"], {message: "The email_comms_type field is a required field. It takes one of the following values: I want all emails, One weekly recap, Stock emails only or Never / Unsubscribe."}).optional(),
-
-    sms_comms: z.boolean("The sms_comms field is a required field. It only accepts boolean values.").optional()
-
-});
-
 const updateUserSchema = z.object({
-    user_id: z.string("The user_id field must be a string. It is a required field.").length(12, {message: "The user_id must be 12 characters long."}).regex(customValidators.twelveCharacterRegex, {message: "The user_id can only contain lowercase letters and numbers."}),
 
     email: customValidators.zodIsEmail.optional(),
     

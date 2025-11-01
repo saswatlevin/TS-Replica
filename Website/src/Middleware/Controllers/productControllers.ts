bbd3f76928c258b1cea9c6ffe52ba3273eaf7dd6
Saswat Levin <saswatlevin@gmail.com>
2025-11-02 02:59:33 +0300

    Indented code in productControllers.js and the ProductImage Mongoose model

--- Website/src/Middleware/Controllers/productControllers.ts
+++ Website/src/Middleware/Controllers/productControllers.ts
@@ -20,35 +20,35 @@ import Product from '../Models/Product';
 
 const createProduct = async (req: any,  res: any, next: any): Promise<void> => {
 
-// Get the request body.
-const request_body = req.body;
+    // Get the request body.
+    const request_body = req.body;
 
-// Check if the request body is empty
-if (checkIsEmptyObject(request_body) === true) {
-    const empty_request_body_error = new EmptyRequestBodyError(`Could not create Product as the request body is empty.`);
-    throw empty_request_body_error;
-}
+    // Check if the request body is empty
+    if (checkIsEmptyObject(request_body) === true) {
+        const empty_request_body_error = new EmptyRequestBodyError(`Could not create Product as the request body is empty.`);
+        throw empty_request_body_error;
+    }
 
-const product_id = createRandomString(6);
+    const product_id = createRandomString(6);
 
-// Set the product_id in the request params
-// This is done to pass it on to checkProduct
-req.params.product_id = product_id;
+    // Set the product_id in the request params
+    // This is done to pass it on to checkProduct
+    req.params.product_id = product_id;
 
-const product_exists = await checkProduct(req, res, next);
+    const product_exists = await checkProduct(req, res, next);
 
-// Check if the product exists
-if (product_exists === false) {
-    const product_not_found_error = new DuplicateDocumentError(`Product document with product_id ${product_id} already exists.`);
-    throw product_not_found_error;
-}
+    // Check if the product exists
+    if (product_exists === false) {
+        const product_not_found_error = new DuplicateDocumentError(`Product document with product_id ${product_id} already exists.`);
+        throw product_not_found_error;
+    }
 
-// Set the docType in the request body
-request_body.docType = "PRODUCT";
+    // Set the docType in the request body
+    request_body.docType = "PRODUCT";
 
-const result = await Product.create(request_body);
+    const result = await Product.create(request_body);
 
-res.status(201).json(result);
+    res.status(201).json(result);
 
 }
 

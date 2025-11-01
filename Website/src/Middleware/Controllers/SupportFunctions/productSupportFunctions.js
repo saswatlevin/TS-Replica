f7c75fd0c516d410ad38ef759bf0a7adb6c988b2
Saswat Levin <saswatlevin@gmail.com>
2025-11-02 02:53:27 +0300

    Changed the CheckProduct function to check for products using product_id and product_name

--- Website/src/Middleware/Controllers/SupportFunctions/productSupportFunctions.js
+++ Website/src/Middleware/Controllers/SupportFunctions/productSupportFunctions.js
@@ -4,8 +4,13 @@ const Product = require('../Models/Product');
 const checkProduct = async(req, res, next) => {
     console.log("In checkProduct");
     const product_id = req.params.product_id;
-    const product_id_object = {product_id: product_id};
-    const result = await Product.find(product_id_object);
+    
+    const product_name = req.body.product_name;
+    
+    const product_search_object = {product_id: product_id, 
+    product_name: product_name};
+    
+    const result = await Product.findOne(product_search_object);
     
     if (result.length === 0) {
        return false;

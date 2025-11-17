45369d3ec95548674028058e78627ccdbddee812
Saswat K. Levin <saswatlevin@gmail.com>
2025-11-17 13:00:57 +0300

    Passed req instead of request_body to updateCartItemPrice in cartItemControllers.js

--- Website/src/Middleware/Controllers/cartItemControllers.js
+++ Website/src/Middleware/Controllers/cartItemControllers.js
@@ -22,13 +22,13 @@ const updateCartItemPrice = async(req, res, next) => {
 
         const user_id = req.params.user_id;
 
-        if (checkIsEmptyObject(request_body) === true) {
+        if (checkIsEmptyObject(req) === true) {
             const empty_request_body_error = new EmptyRequestBodyError(`Could not update Cart Item with cart_item_id ${cart_item_id} as the request body is empty.`);
             throw empty_request_body_error;
         }
     
 
-        if (checkUserExists(request_body) === false) {
+        if (checkUserExists(req) === false) {
             const user_id_not_found_error = new ResourceNotFoundError(`Could not update the Cart Item price since the user with user_id ${user_id} does not exist.`);
             throw user_id_not_found_error;
         }

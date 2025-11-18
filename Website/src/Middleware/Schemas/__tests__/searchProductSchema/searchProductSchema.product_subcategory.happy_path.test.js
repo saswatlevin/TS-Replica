d624c1b2f3d27da2ccba988c27610b561c6ff939
Saswat K. Levin <saswatlevin@gmail.com>
2025-11-18 13:09:34 +0300

    Added and succesfully ran all tests for searchProductSchema after keeping only product_color, product_name and product_subcategory in the testData

--- Website/src/Middleware/Schemas/__tests__/searchProductSchema/searchProductSchema.product_subcategory.happy_path.test.js
+++ Website/src/Middleware/Schemas/__tests__/searchProductSchema/searchProductSchema.product_subcategory.happy_path.test.js
@@ -10,31 +10,7 @@ describe('searchProductSchema - product_subcategory - HAPPY PATH TEST', () => {
   
             product_color: "Bark Plaid Linen",
   
-            product_description: "Rugged by design, built with comfort in mind. The Craftsman Shirt pairs classic workwear detailing with a breathable cotton-linen canvas that’s made to handle the season’s shifting demands. Durable, dependable, and crafted to keep up for years to come.",
-  
-            product_price: 128,
-            
-            product_category: "Upper Garment",
-  
-            product_subcategory: "Shirt",
-  
-            product_subcategory_type: "Long-Sleeved Shirt",
-  
-            product_fit: "Fitted at the chest with a straighter fit through the body. Shorter tail length to be worn untucked. Model is 6'3\", wearing a Medium.",
-  
-            product_garment_weight: {
-                garment_weight_description: "Your daily driver, at a versatile all-season weight.",
-                garment_weight: "Medium"
-            },
-  
-            product_material: "Crafted from a mid-weight organic cotton and linen canvas and washed for a soft, lived-in feel. Features classic double chest pockets—complete with a handy pen sleeve—and reinforced with double-needle felled seams for lasting durability.",
-            
-            product_supply_type: {
-                supply_type_description: "This product is part of a small batch manufacturing run that may use exclusive materials like dead stock fabrics. The product is limited in quantity and may never be in stock again. Limited products are available for immediate shipping.",
-                supply_type: "Limited"
-            },
-            
-            product_specifications: "7-oz. 55% linen, 45% organic cotton canvas. Washed for a soft, lived in feel. Two U-shaped chest pockets with button through flaps. Left pocket features a pen sleeve. Double-needle felled construction. Wash cold and lay flat to dry. Made in China."
+            product_subcategory: "Shirt"         
         };
 
         // Act
@@ -63,31 +39,7 @@ describe('searchProductSchema - product_subcategory - HAPPY PATH TEST', () => {
   
             product_color: "Bark Plaid Linen",
   
-            product_description: "Rugged by design, built with comfort in mind. The Craftsman Shirt pairs classic workwear detailing with a breathable cotton-linen canvas that’s made to handle the season’s shifting demands. Durable, dependable, and crafted to keep up for years to come.",
-  
-            product_price: 128,
-            
-            product_category: "Upper Garment",
-  
-            product_subcategory: "Bottom",
-  
-            product_subcategory_type: "Long-Sleeved Shirt",
-  
-            product_fit: "Fitted at the chest with a straighter fit through the body. Shorter tail length to be worn untucked. Model is 6'3\", wearing a Medium.",
-  
-            product_garment_weight: {
-                garment_weight_description: "Your daily driver, at a versatile all-season weight.",
-                garment_weight: "Medium"
-            },
-  
-            product_material: "Crafted from a mid-weight organic cotton and linen canvas and washed for a soft, lived-in feel. Features classic double chest pockets—complete with a handy pen sleeve—and reinforced with double-needle felled seams for lasting durability.",
-            
-            product_supply_type: {
-                supply_type_description: "This product is part of a small batch manufacturing run that may use exclusive materials like dead stock fabrics. The product is limited in quantity and may never be in stock again. Limited products are available for immediate shipping.",
-                supply_type: "Limited"
-            },
-            
-            product_specifications: "7-oz. 55% linen, 45% organic cotton canvas. Washed for a soft, lived in feel. Two U-shaped chest pockets with button through flaps. Left pocket features a pen sleeve. Double-needle felled construction. Wash cold and lay flat to dry. Made in China."
+            product_subcategory: "Bottom"
         };
 
         // Act

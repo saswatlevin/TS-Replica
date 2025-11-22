bd60afd2797f58531e6eb613641bb0f9c1d23fe0
Saswat K. Levin <saswatlevin@gmail.com>
2025-11-23 01:35:27 +0300

    Added cartItemResponseSchema

--- Website/src/Middleware/Schemas/cartItemSchemas.js
+++ Website/src/Middleware/Schemas/cartItemSchemas.js
@@ -1,10 +1,9 @@
 const z = require('zod');
 const customValidators = require('../Validators/CustomValidators/customFormatValidators');
 const cartItemValidators = require('../Validators/CustomValidators/cartItemValidators');
+const objectIdSchema = require('./objectIdSchema');
 
-const createCartItemSchema = z.object({
-    cart_item_id: cartItemValidators.zodIscartItemId,
-
+const cartItemRequestSchema = z.object({
     product_id: customValidators.zodIsProductId,
     
     sku: customValidators.zodIsSKU,
@@ -18,7 +17,25 @@ const createCartItemSchema = z.object({
     cart_item_quantity: cartItemValidators.zodIsCartItemQuantity
 });
 
-const cartItemArraySchema = z.array(createCartItemSchema).min(0); 
+const cartItemResponseSchema = z.object({
+    cart_item_id: cartItemValidators.zodIscartItemId,
+
+    product_id: customValidators.zodIsProductId,
+    
+    sku: customValidators.zodIsSKU,
+    
+    cart_item_name: cartItemValidators.zodIsCartItemName,
+
+    cart_item_price: cartItemValidators.zodIsCartItemPrice,
+    
+    cart_item_image_uri: cartItemValidators.zodIsCartItemImageURI,
+    
+    cart_item_quantity: cartItemValidators.zodIsCartItemQuantity,
+
+    _id: objectIdSchema
+});
+
+const cartItemArraySchema = z.array(cartItemRequestSchema).min(0); 
 
 const updateCartItemPriceSchema = z.object({
     cart_item_price: cartItemValidators.zodIsCartItemPrice
@@ -26,7 +43,8 @@ const updateCartItemPriceSchema = z.object({
 
 
 module.exports = {
-    createCartItemSchema,
+    cartItemRequestSchema,
+    cartItemResponseSchema,
     cartItemArraySchema,
     updateCartItemPriceSchema
 };

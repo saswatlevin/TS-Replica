71ba6da45ba9132105b4fb5f5de91a951f7e6644
Saswat K. Levin <saswatlevin@gmail.com>
2025-11-24 10:01:26 +0300

    Mostly linebreaks in productControllers.js

--- Website/src/Middleware/Controllers/productControllers.js
+++ Website/src/Middleware/Controllers/productControllers.js
@@ -12,11 +12,12 @@ const { checkProductValueExists } = require('./SupportFunctions/productSupportFu
 const { checkProductGarmentWeightValueExists } = require('./SupportFunctions/productSupportFunctions');
 const { checkProductSupplyTypeValueExists } = require('./SupportFunctions/productSupportFunctions');
 const DuplicateDocumentError = require('../OperationalErrors/DuplicateDocumentError');
+const { updateCartItemPrice } = require('./cartItemControllers');
 const Product = require('../Models/Product');
 
-const createProduct = asyncErrorHandler(async(req, res, next) => {
+const createProduct = asyncErrorHandler(async (req, res, next) => {
     console.log("In createProduct");
-    
+
     // Get the request body.
 
     const request_body_deep_clone = JSON.parse(JSON.stringify(req.body));
@@ -24,7 +25,7 @@ const createProduct = asyncErrorHandler(async(req, res, next) => {
     console.log("request_body_deep_clone before modification ", request_body_deep_clone);
     //console.log("request_body_deep_clone.ProductItems before modification ", request_body_deep_clone.product_items[0]);
     //console.log("request_body_deep_clone.ProductImages before modification ", request_body_deep_clone.product_images[0]);
-    
+
 
     // Check if the request body is empty
     console.log("Checking if the request body is empty");
@@ -50,15 +51,15 @@ const createProduct = asyncErrorHandler(async(req, res, next) => {
     // Generate the image_id
     const image_id = createRandomString(6);
     console.log("Generated image_id ", image_id);
-    
+
     const doc_type = "PRODUCT";
     console.log("Generated doc_type ", doc_type);
 
-    request_body_deep_clone.product_items[0] = {sku: sku, ...request_body_deep_clone.product_items[0]};
-    request_body_deep_clone.product_images[0] = {image_id: image_id, ...request_body_deep_clone.product_images[0]};
+    request_body_deep_clone.product_items[0] = { sku: sku, ...request_body_deep_clone.product_items[0] };
+    request_body_deep_clone.product_images[0] = { image_id: image_id, ...request_body_deep_clone.product_images[0] };
 
     console.log("request_body_deep_clone after modification ", request_body_deep_clone);
-    
+
 
     // Create the porduct object
     const product = {
@@ -67,7 +68,7 @@ const createProduct = asyncErrorHandler(async(req, res, next) => {
         ...request_body_deep_clone
     };
     console.log("The product object to be created ", product);
-    
+
     const result = await Product.create(product);
 
     const final_result = result.toObject({
@@ -76,7 +77,7 @@ const createProduct = asyncErrorHandler(async(req, res, next) => {
         minimize: false,
         versionKey: true,
         flattenMaps: true
-      });
+    });
     console.log("final_result ", final_result);
 
     console.log("Sending the result to the client as JSON with status 201");
@@ -84,7 +85,7 @@ const createProduct = asyncErrorHandler(async(req, res, next) => {
 
 });
 
-const updateProductPrice = asyncErrorHandler(async(req, res, next) => {
+const updateProductPrice = asyncErrorHandler(async (req, res, next) => {
     console.log("In updateProduct");
 
     console.log("Checking if the request body is empty");
@@ -96,11 +97,11 @@ const updateProductPrice = asyncErrorHandler(async(req, res, next) => {
 
     const request_body_deep_clone = JSON.parse(JSON.stringify(req.body));
     console.log("request_body_deep_clone ", request_body_deep_clone);
-    
+
     const product_id = req.params.product_id;
     console.log("Getting the product_id from the request params ", product_id);
 
-    
+
     const product_exists = await checkProduct(req, res, next);
     console.log("Checking if the product exists ", product_exists);
 
@@ -109,20 +110,20 @@ const updateProductPrice = asyncErrorHandler(async(req, res, next) => {
         throw product_not_found_error;
     }
 
-    const filter = {product_id: product_id};
+    const filter = { product_id: product_id };
     console.log("filter ", filter);
 
     const update_object = request_body_deep_clone;
     console.log("update_object ", update_object);
 
 
-    const update_product_price_result = await Product.findOneAndUpdate(filter, update_object, {new: true}, {runValidators: true}).lean();
+    const update_product_price_result = await Product.findOneAndUpdate(filter, update_object, { new: true }, { runValidators: true }).lean();
     console.log("update_product_price_result ", update_product_price_result);
 
     req.params.updated_product_price = update_product_price_result.product_price;
     console.log("Storing the updated_product_price in the request params ", req.params.updated_product_price);
 
-    const update_cart_item_price_result = await updateCartItemPrice(req, res, next);
+    const update_cart_item_price_result = await updateCartItemPrice(req);
     console.log("update_cart_item_price_result ", update_cart_item_price_result);
 
     const result_array = [update_product_price_result, update_cart_item_price_result];
@@ -132,7 +133,7 @@ const updateProductPrice = asyncErrorHandler(async(req, res, next) => {
     res.status(200).json(result_array);
 });
 
-const updateProduct = asyncErrorHandler(async(req, res, next) => {
+const updateProduct = asyncErrorHandler(async (req, res, next) => {
     console.log("In updateProduct");
 
     const product_id = req.params.product_id;
@@ -143,7 +144,7 @@ const updateProduct = asyncErrorHandler(async(req, res, next) => {
         const empty_request_body_error = new EmptyRequestBodyError(`Could not update Product with product_id ${product_id} as the request body is empty.`);
         throw empty_request_body_error;
     }
-    
+
     console.log("Checking if the product exists");
     if (await checkProduct(req) === false) {
         const product_not_found_error = new ResourceNotFoundError(`Could not update Product documentwith product_id ${product_id} since it does not exist.`);
@@ -151,7 +152,7 @@ const updateProduct = asyncErrorHandler(async(req, res, next) => {
     }
 
     console.log("Checking if the product value to be updated already exists");
-    
+
     if (await checkProductValueExists(req) === true) {
         const product_value_error = new CustomError(`Could not update Product document with product_id ${product_id} since the value(s) ${req.body} already exist(s).`, 400);
         throw product_value_error;
@@ -161,24 +162,24 @@ const updateProduct = asyncErrorHandler(async(req, res, next) => {
     const request_body_deep_clone = JSON.parse(JSON.stringify(req.body));
     console.log("Got the request_body_deep_clone ", request_body_deep_clone);
 
-    const filter = {product_id: product_id};
+    const filter = { product_id: product_id };
     console.log("filter ", filter);
 
     const update_object = request_body_deep_clone;
     console.log("update_object ", update_object);
 
-    console.log("Calling findOneAndUpdate to update the product document");    
-    const result = await Product.findOneAndUpdate(filter, update_object, {new: true}, {runValidators: true}).lean();
+    console.log("Calling findOneAndUpdate to update the product document");
+    const result = await Product.findOneAndUpdate(filter, update_object, { new: true }, { runValidators: true }).lean();
 
 
     console.log("The updated product document, result ", result);
 
     console.log("Sending the result to the client as JSON with status 200");
     res.status(200).json(result);
-    
+
 });
 
-const updateProductGarmentWeight = asyncErrorHandler(async(req, res, next) => {
+const updateProductGarmentWeight = asyncErrorHandler(async (req, res, next) => {
     console.log("In updateProductGarmentWeight");
     const product_id = req.params.product_id;
     console.log("Got the product_id from the request params ", product_id);
@@ -188,15 +189,15 @@ const updateProductGarmentWeight = asyncErrorHandler(async(req, res, next) => {
         const empty_request_body_error = new EmptyRequestBodyError(`Could not update Product with product_id ${product_id} as the request body is empty.`);
         throw empty_request_body_error;
     }
-    
+
     console.log("Checking if the product exists");
     if (await checkProduct(req) === false) {
         const product_not_found_error = new ResourceNotFoundError(`Could not update Product 
         document with product_id ${product_id} since it does not exist.`);
         throw product_not_found_error;
     }
-    
-    
+
+
     const garment_weight_value_exists = await checkProductGarmentWeightValueExists(req);
     console.log("garment_weight_value_exists ", garment_weight_value_exists);
 
@@ -208,14 +209,14 @@ const updateProductGarmentWeight = asyncErrorHandler(async(req, res, next) => {
     const request_body_deep_clone = JSON.parse(JSON.stringify(req.body));
     console.log("Got the request_body_deep_clone ", request_body_deep_clone);
 
-    const filter = {product_id: product_id};
+    const filter = { product_id: product_id };
     console.log("filter ", filter);
 
-    const update_object = {"product_garment_weight": request_body_deep_clone};
+    const update_object = { "product_garment_weight": request_body_deep_clone };
     console.log("update_object ", update_object);
 
-    console.log("Calling findOneAndUpdate to update the product document");    
-    const result = await Product.findOneAndUpdate(filter, update_object, {new: true}, {runValidators: true}).lean();
+    console.log("Calling findOneAndUpdate to update the product document");
+    const result = await Product.findOneAndUpdate(filter, update_object, { new: true }, { runValidators: true }).lean();
     console.log("The updated product document, result ", result);
 
     console.log("Sending the result to the client as JSON with status 200");
@@ -223,7 +224,7 @@ const updateProductGarmentWeight = asyncErrorHandler(async(req, res, next) => {
 
 });
 
-const updateProductSupplyType = asyncErrorHandler(async(req, res, next) => {
+const updateProductSupplyType = asyncErrorHandler(async (req, res, next) => {
     console.log("In updateProductSupplyType");
 
     const product_id = req.params.product_id;
@@ -237,10 +238,10 @@ const updateProductSupplyType = asyncErrorHandler(async(req, res, next) => {
 
     console.log("Checking if the product exists");
     if (await checkProduct(req) === false) {
-        const product_not_found_error = new ResourceNotFoundError(`Could not update Product document with product_id ${product_id} since it does not exist.`);  
+        const product_not_found_error = new ResourceNotFoundError(`Could not update Product document with product_id ${product_id} since it does not exist.`);
         throw product_not_found_error;
     }
-    
+
     console.log("Checking if the product supply type value to be updated already exists");
     if (await checkProductSupplyTypeValueExists(req) === true) {
         const product_supply_type_value_error = new CustomError(`Could not update Product document with product_id ${product_id} since the product_supply_type value(s) already exist(s).`, 400);
@@ -250,21 +251,21 @@ const updateProductSupplyType = asyncErrorHandler(async(req, res, next) => {
     const request_body_deep_clone = JSON.parse(JSON.stringify(req.body));
     console.log("Got the request_body_deep_clone ", request_body_deep_clone);
 
-    const filter = {product_id: product_id};
+    const filter = { product_id: product_id };
     console.log("filter ", filter);
 
-    const update_object = {"product_supply_type": request_body_deep_clone};
+    const update_object = { "product_supply_type": request_body_deep_clone };
     console.log("update_object ", update_object);
 
-    console.log("Calling findOneAndUpdate to update the product document");    
-    const result = await Product.findOneAndUpdate(filter, update_object, {new: true}, {runValidators: true}).lean();
+    console.log("Calling findOneAndUpdate to update the product document");
+    const result = await Product.findOneAndUpdate(filter, update_object, { new: true }, { runValidators: true }).lean();
     console.log("The updated product document, result ", result);
 
     console.log("Sending the result to the client as JSON with status 200");
     res.status(200).json(result);
 });
 
-const searchProducts = asyncErrorHandler(async(req, res, next) => {
+const searchProducts = asyncErrorHandler(async (req, res, next) => {
     console.log("In searchProducts");
 
     console.log("Checking if the request body is empty");

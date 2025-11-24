30f8cd5c51d5a6e6f9d4c10a8de37b1523160b29
Saswat K. Levin <saswatlevin@gmail.com>
2025-11-24 10:12:02 +0300

    Added logic to ensure that no decimal precision is added to date time provided by currentDate().

--- Website/src/Middleware/getCurrentDateTime.js
+++ Website/src/Middleware/getCurrentDateTime.js
@@ -1,8 +1,8 @@
 const getCurrentDateTime = () => {
     console.log("In getCurrentDateTime ");
-    
+
     const currentDate = new Date();
-    const iso8601DateTimeString = currentDate.toISOString();
+    const iso8601DateTimeString = currentDate.toISOString().slice(0, 19);
 
     return iso8601DateTimeString;
 };

c6f63f0255b7d0f894dd76669ea73488f8e83796
Saswat K. Levin <saswatlevin@gmail.com>
2025-11-25 21:23:11 +0300

    Added console.log statements, and request_body_deep_clone to all controllers in shippingAddressControllers.js

--- Website/src/Middleware/Controllers/shippingAddressControllers.js
+++ Website/src/Middleware/Controllers/shippingAddressControllers.js
@@ -14,44 +14,44 @@ const createShippingAddress = asyncErrorHandler(async (req, res, next) => {
 
     const user_id = req.params.user_id;
 
+    console.log("Checking if the request body is empty or not");
     if (checkIsEmptyObject(req) === true) {
         const empty_request_body_error = new EmptyRequestBodyError(`Could not create shipping address for user with user_id ${user_id} as the request body is empty.`);
 
         throw empty_request_body_error;
     }
 
-    // Check if the user exists
-    const user_exists = await checkUserExists(req);
-
-    //console.log("user_exists ", user_exists);
-
-    // Deep clone the request body
-    var request_body = JSON.parse(JSON.stringify(req.body));
-
-    // Generate the shipping_address_id
-    const shipping_address_id = createRandomString(6);
-
-    // Create the shipping address object
-    const shipping_address = {
-        shipping_address_id: shipping_address_id,
-        ...request_body
-    };
-
-    console.log("shipping_address ", shipping_address);
-
-    if (user_exists === false) {
+    console.log("Checking if the user exists or not");
+    if (await checkUserExists(req) === false) {
         const error = new ResourceNotFoundError(`Could not create the shipping address since the user with user_id ${user_id} does not exist.`);
         throw error;
     }
 
-    else {
-        // A live mongoose document contains many hiiden keys and fields. 
-        // The .lean() functions strips these keys and fields completely.
-        const result = await User.findOneAndUpdate({ user_id: user_id }, { $push: { ShippingAddresses: shipping_address } }, { new: true }).lean(); // return the updated document;
-        console.log("Shipping address created successfully, result ", result);
-        // Here, we use 201 since it means that the resource has been created.
-        res.status(201).json(result);
+    // Deep clone the request body
+    const request_body_deep_clone = JSON.parse(JSON.stringify(req.body));
+    console.log("request_body_deep_clone ", request_body_deep_clone);
+
+    // Generate the shipping_address_id
+    const shipping_address_id = createRandomString(6);
+    console.log("shipping_address_id ", shipping_address_id);
+
+    // Create the shipping address object
+    const shipping_address = {
+        shipping_address_id: shipping_address_id,
+        ...request_body_deep_clone
     }
+    
+    console.log("shipping_address ", shipping_address);
+
+    // A live mongoose document contains many hiiden keys and fields. 
+    // The .lean() functions strips these keys and fields completely.
+    const result = await User.findOneAndUpdate({ user_id: user_id }, { $push: { ShippingAddresses: shipping_address } }, { new: true }, {runValidators: true}).lean(); // return the updated document;
+    console.log("result ", result);
+    
+    // Here, we use 201 since it means that the resource has been created.
+    res.status(201).json(result);
+    console.log("===END OF createShippingAddress===");
+    
 
 });
 
@@ -59,20 +59,26 @@ const updateShippingAddress = asyncErrorHandler(async (req, res, next) => {
     console.log("In updateShippingAddress");
 
     const user_id = req.params.user_id;
+    console.log("user_id ", user_id);
 
     const shipping_address_id = req.params.shipping_address_id;
+    console.log("shipping_address_id ", shipping_address_id);
 
+
+    console.log("Checking if the request body is empty or not");
     if (checkIsEmptyObject(req) === true) {
         const empty_request_body_error = new EmptyRequestBodyError(`Could not update shipping address with shipping_address_id ${shipping_address_id} for user with user_id ${user_id} as the request body is empty.`);
 
         throw empty_request_body_error;
     }
 
+    console.log("Checking if the user exists or not");
     if (await checkUserExists(req) === false) {
         const user_id_not_found_error = new ResourceNotFoundError(`Could not update the shipping address since the user with user_id ${user_id} does not exist.`);
         throw user_id_not_found_error;
     }
 
+    console.log("Check whether the shipping address exists or not");
     if (await checkShippingAddressExists(req) === false) {
         const shipping_address_not_found_error = new ResourceNotFoundError(`Could not update the shipping address with ${shipping_address_id} since it does not exist.`);
         throw shipping_address_not_found_error;
@@ -81,23 +87,15 @@ const updateShippingAddress = asyncErrorHandler(async (req, res, next) => {
     // The user_id of the User document where the shipping address is to be updated.
     // The shipping_address_id of the shipping_address to be updated.
     const shipping_address_selector = { user_id: user_id, "ShippingAddresses.shipping_address_id": shipping_address_id };
+    console.log("shipping_address_selector ", shipping_address_selector);
 
-    const request_body = req.body;
-
-    console.log("request_body in updateShippingAddress ", request_body);
-
-    // Here, we check if the request body is empty or not
-    /*if (checkIsEmptyObject(request_body) === true) {
-        const empty_request_body_error = new EmptyRequestBodyError(`Could not update the shipping address with shipping_address_id ${shipping_address_id} of user with user_id ${user_id} due to empty request body.`);
-        throw empty_request_body_error; 
-    }*/
+    const request_body_deep_clone = JSON.parse(JSON.stringify(req.body));
+    console.log("request_body_deep_clone ", request_body_deep_clone);
 
     // Building the shipping address update request body.
     const update_request = Object.fromEntries(
-        Object.entries(request_body).map(([key, value]) => [`ShippingAddresses.$.${key}`, value])
+        Object.entries(request_body_deep_clone).map(([key, value]) => [`ShippingAddresses.$.${key}`, value])
     );
-
-    console.log("shipping_address_selector ", shipping_address_selector);
 
     console.log("update_request ", update_request);
 
@@ -111,7 +109,8 @@ const updateShippingAddress = asyncErrorHandler(async (req, res, next) => {
             $set: update_request
         }
     ).lean();
-    console.log("shipping address update result ", result);
+    
+    console.log("result ", result);
 
     if (result["modifiedCount"] === 1 && result["matchedCount"] === 1) {
         // OK
@@ -125,28 +124,7 @@ const updateShippingAddress = asyncErrorHandler(async (req, res, next) => {
         res.status(200).json(result);
     }
 
-
-
-
-    // The positional operator, $, holds the matched position in the array.
-    // The $set operator makes only the specified elements go to the server.
-    // The $set operator replaces the value of a field with the specified value.
-
-    /**
-     * If the field does not exist, $set will add a new field with the specified value, provided that the new field does not violate a type constraint. If you specify a dotted path for a non-existent field, $set will create the embedded documents as needed to fulfill the dotted path to the field.
-    
-     * If you specify multiple field-value pairs, $set will update or create each field.
-     **/
-
-    // The Object.entries() static method returns an array of a given object's own enumerable string-keyed property key-value pairs.
-    // The Object.fromEntries() static method transforms a list of key-value pairs into an object.
-
-    // Here, what we are doing is: we are passing the request body to the Object.entries() function.
-    // The object.entries() function converts the request body from {"key": "value"} / {"key": value} to [key, "value"] / [key, value].
-    // It then maps each [key, "value"] / [key, value] pair to [ShippingAddresses.$.key, "value"] / [ShippingAddresses.$.key, "value"].
-    // The Object.FromEntries() function then converts each [ShippingAddresses.$.key, "value"] / [ShippingAddresses.$.key, value] pair to key:"value" / key:value pair and then puts all of them in a single JS object.
-
-
+    console.log("===END OF updateShippingAddress===");
 
 
 });
@@ -155,19 +133,23 @@ const getShippingAddressById = asyncErrorHandler(async (req, res, next) => {
     console.log("In getShippingAddressById");
 
     const user_id = req.params.user_id;
+    console.log("user_id ", user_id);
 
+    console.log("Check if the request body is empty");
     if (checkIsEmptyObject(req) === true) {
         const empty_request_body_error = new EmptyRequestBodyError(`Could not get shipping address for user with user_id ${user_id} as the request body is empty.`);
 
         throw empty_request_body_error;
     }
 
+    console.log("Check if the user exists or not");
     if (await checkUserExists(req) === false) {
         const user_id_not_found_error = new ResourceNotFoundError(`Could not find the shipping address since the user with user_id ${user_id} does not exist.`);
         throw user_id_not_found_error;
     }
 
     const shipping_address_id = req.body.shipping_address_id;
+    console.log("shipping_address_id ", shipping_address_id);
 
     // Aggregation pipeline to get the shipping_address sub-document
     /**
@@ -203,7 +185,7 @@ const getShippingAddressById = asyncErrorHandler(async (req, res, next) => {
     // Returns [] if resource not found
     const result = await User.aggregate(shipping_address_aggregation_pipeline);
 
-    //console.log("Shipping Address returned from getShippingAddressById ", result);
+    console.log("result ", result);
     //console.log("result[0] ", result[0]);
 
     if (result.length === 1) {
@@ -217,6 +199,8 @@ const getShippingAddressById = asyncErrorHandler(async (req, res, next) => {
         const shipping_address_not_found_error = new ResourceNotFoundError(`Could not find the shipping address with ${shipping_address_id} since it does not exist.`);
         throw shipping_address_not_found_error;
     }
+
+    console.log("===END OF getShippingAddressById===");
 
 });
 
@@ -237,14 +221,14 @@ const searchShippingAddress = asyncErrorHandler(async (req, res, next) => {
     }
 
     // Make a deep clone of the request body
-    const request_body = _.cloneDeep(req.body);
+    const request_body_deep_clone = _.cloneDeep(req.body);
 
-    console.log("request_body in search_shipping_address is ", request_body);
+    console.log("request_body in search_shipping_address is ", request_body_deep_clone);
 
 
     // Building the shipping address search query
     const shipping_address_search_query = Object.fromEntries(
-        Object.entries(request_body).map(([key, value]) => [`ShippingAddresses.${key}`, value])
+        Object.entries(request_body_deep_clone).map(([key, value]) => [`ShippingAddresses.${key}`, value])
     );
 
 
@@ -291,6 +275,8 @@ const searchShippingAddress = asyncErrorHandler(async (req, res, next) => {
         throw shipping_address_not_found_error;
     }
 
+    console.log("===END OF searchShippingAddress===");
+
 });
 
 const deleteShippingAddressById = asyncErrorHandler(async (req, res, next) => {
@@ -298,25 +284,19 @@ const deleteShippingAddressById = asyncErrorHandler(async (req, res, next) => {
 
     const user_id = req.params.user_id;
 
-    const request_body = req.body;
-
-    const user_exists = await checkUserExists(req);
-
-    console.log("user_exists ", user_exists);
-
-    if (user_exists === false) {
-        const empty_request_body_error = new EmptyRequestBodyError(`Could not find the shipping address to be deleted, of user with user_id ${user_id} due to empty request body.`);
+    console.log("Checking if the request body is empty or not");
+    if (checkIsEmptyObject(req) === true) {
+        const empty_request_body_error = new EmptyRequestBodyError(`Could not delete the shipping address that belongs to the user with ${user_id} since the request body is empty.`);
         throw empty_request_body_error;
     }
 
-    // Check if checkUserExists returns an error
-    else if (user_exists !== true) {
-        throw user_exists;
+    const request_body_deep_clone = JSON.parse(JSON.stringify(req.body));
+    console.log("request_body_deep_clone ", request_body_deep_clone);
 
-    }
-
-    else {
-        ;
+    console.log("Checking if the user exists or not");
+    if (await checkUserExists(req) === false) {
+        const resource_not_found_error = new ResourceNotFoundError(`Could not find the shipping address to be deleted, of user with user_id ${user_id} due as the user doesn't exist.`);
+        throw resource_not_found_error;
     }
 
 
@@ -327,10 +307,13 @@ const deleteShippingAddressById = asyncErrorHandler(async (req, res, next) => {
         throw shipping_address_not_found_error;
     }*/
 
-    const deletion_result = await User.updateOne({ user_id: user_id }, { $pull: { "ShippingAddresses": request_body } });
+    const result = await User.updateOne({ user_id: user_id }, { $pull: { "ShippingAddresses": request_body_deep_clone } });
+    console.log("result ", result);
 
     // Here 200 is used as status code since the server wants to send a message after deletion.
-    res.status(200).json(deletion_result);
+    res.status(200).json(result);
+
+    console.log("===END OF deleteShippingAddressById===");
 
 });
 

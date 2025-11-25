31ba63f6a899c5f33796332ca2bf011787bedcf1
Saswat K. Levin <saswatlevin@gmail.com>
2025-11-25 23:06:36 +0300

    Added :user_id to updateUser and updateUserPassword routes in userRoutes.js

--- Website/src/Middleware/Routes/shippingAddressRoutes.js
+++ Website/src/Middleware/Routes/shippingAddressRoutes.js
@@ -5,7 +5,7 @@ const requestValidator = require('../Validators/requestValidator');
 const shippingAddressSchemas = require('../Schemas/shippingAddressSchemas');
 const { xss } = require('express-xss-sanitizer');
 
-router.route('/createshippingaddress/:user_id').post(requestValidator(shippingAddressSchemas.createShippingAddressRequestSchema), xss(), shippingAddressControllers.createShippingAddress);
+router.route('/createshippingaddress/:user_id').post(requestValidator(shippingAddressSchemas.shippingAddressRequestSchema), xss(), shippingAddressControllers.createShippingAddress);
 
 router.route('/updateshippingaddress/:user_id/:shipping_address_id').patch(requestValidator(shippingAddressSchemas.updateShippingAddressRequestSchema), xss(), shippingAddressControllers.updateShippingAddress);
 
--- Website/src/Middleware/Routes/userRoutes.js
+++ Website/src/Middleware/Routes/userRoutes.js
@@ -6,8 +6,8 @@ const userSchemas = require('../Schemas/userSchemas');
 const { xss } = require('express-xss-sanitizer');
 
 router.route('/registeruser').post(requestValidator(userSchemas.userRequestSchema), xss(), userControllers.registerUser);
-router.route('/updateuser').patch(requestValidator(userSchemas.updateUserSchema),  xss(), userControllers.updateUser);
-router.route('/updateuserpassword').patch(requestValidator(userSchemas.updateUserPasswordSchema),  xss(), userControllers.updateUserPassword);
+router.route('/updateuser/:user_id').patch(requestValidator(userSchemas.updateUserSchema),  xss(), userControllers.updateUser);
+router.route('/updateuserpassword/:user_id').patch(requestValidator(userSchemas.updateUserPasswordSchema),  xss(), userControllers.updateUserPassword);
 router.route('/getuserbyid').get(requestValidator(userSchemas.getUserByIdSchema),  xss(), userControllers.getUserById);
 router.route('/getusersbyname').get(requestValidator(userSchemas.getUsersByNameSchema),  xss(), userControllers.searchUsersByName);
 //router.route('/objectidtest').get(userControllers.objectIdTest);

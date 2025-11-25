e00c127740d6e0d26dcfc7cc7ca9be5e05d8bb5e
Saswat K. Levin <saswatlevin@gmail.com>
2025-11-25 22:01:17 +0300

    Removed product_id from the router.route in cartItemsRoute.js

--- Website/src/Middleware/Routes/cartItemRoutes.js
+++ Website/src/Middleware/Routes/cartItemRoutes.js
@@ -5,6 +5,6 @@ const requestValidator = require('../Validators/requestValidator');
 const cartItemSchemas = require('../Schemas/cartItemSchemas');
 const { xss } = require('express-xss-sanitizer');
 
-router.route('/createcartitem/:product_id/:user_id').post(requestValidator(cartItemSchemas.cartItemRequestSchema), xss(), cartItemControllers.createCartItem);
+router.route('/createcartitem/:user_id').post(requestValidator(cartItemSchemas.cartItemRequestSchema), xss(), cartItemControllers.createCartItem);
 
 module.exports = router;

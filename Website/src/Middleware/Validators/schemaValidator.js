48effb27d6387b8ca4039750adcd721e8475e81a
Saswat K. Levin <saswatlevin@gmail.com>
2025-11-25 21:04:35 +0300

    Added logic to display ZodErrors in schemaValidator.js

--- Website/src/Middleware/Validators/schemaValidator.js
+++ Website/src/Middleware/Validators/schemaValidator.js
@@ -2,9 +2,15 @@ const z = require('zod');
 
 const schemaValidator = (schema, object) => {
     console.log("In schemaValidator");
-    const schemaValidationResult = schema.parse(object);
-    //console.log("schemaValidationResult ", schemaValidationResult);
-    return schemaValidationResult;
+    //console.log("##DEBUG - object ", object);
+    const result = schema.parse(object);
+    //console.log("schema validation result ", result);
+    
+    if (!result.success){
+        console.log("schema validation errors - ", result?.error?.errors);
+    }
+    
+    return result;
     
 }
 

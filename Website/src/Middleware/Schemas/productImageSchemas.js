cce88c8c7a2720868892db57ed723bc152a75858
Saswat K. Levin <saswatlevin@gmail.com>
2025-12-03 02:10:43 +0300

    Removed _id from productImageArrayResponseSchema. Removed productImageArraySchema and productImageSchema too from productImagesSchemas.js

--- Website/src/Middleware/Schemas/productImageSchemas.js
+++ Website/src/Middleware/Schemas/productImageSchemas.js
@@ -1,8 +1,7 @@
 const z = require('zod');
 const customValidators = require('../Validators/CustomValidators/customFormatValidators');
-const objectIdSchema = require('./objectIdSchema');
 
-const productImageSchema = z.object({
+const productImageResponseSchema = z.object({
     image_id: z.string("The image_id field must be a string. It is a required field.").length(12, {message: "The image_id must be 12 characters long."}).regex(customValidators.twelveCharacterRegex, {message: "The image_id can only contain lowercase letters and numbers."}),
 
     image_uri: z.string("The image_uri field must be a string.").min(1, {message: "The image_uri field is a required field."}).max(256, {message: "The image_uri field has a maximum permitted length of 256 characters."}).regex(customValidators.windowsFilePathRegex, {message: "The image_uri path must be a valid windows filepath."}),
@@ -16,25 +15,11 @@ const productImageRequestSchema = z.object({
     main_image: z.boolean("The main_image field must be a boolean value. The main_image field is a required field.")
 }).strict();
 
-const productImageResponseSchema = z.object({
-    image_id: z.string("The image_id field must be a string. It is a required field.").length(12, {message: "The image_id must be 12 characters long."}).regex(customValidators.twelveCharacterRegex, {message: "The image_id can only contain lowercase letters and numbers."}),
-
-    image_uri: z.string("The image_uri field must be a string.").min(1, {message: "The image_uri field is a required field."}).max(256, {message: "The image_uri field has a maximum permitted length of 256 characters."}).regex(customValidators.windowsFilePathRegex, {message: "The image_uri path must be a valid windows filepath."}),
-    
-    main_image: z.boolean("The main_image field must be a boolean value. The main_image field is a required field."),
-    
-    _id: objectIdSchema
-}).strict();
-
-const productImageArraySchema = z.array(productImageSchema).min(1, {message: "At least one product image must be provided."}).max(5, {message: "A maximum of 5 product images can be provided."});
+const productImageArrayResponseSchema = z.array(productImageResponseSchema).min(1, {message: "At least one product image must be provided."}).max(5, {message: "A maximum of 5 product images can be provided."});
 
 const productImageArrayRequestSchema = z.array(productImageRequestSchema).min(1, {message: "At least one product image must be provided."}).max(5, {message: "A maximum of 5 product images can be provided."});
 
-const productImageArrayResponseSchema = z.array(productImageResponseSchema).min(1, {message: "At least one product image must be provided."}).max(5, {message: "A maximum of 5 product images can be provided."});
-
 module.exports = {
-    productImageArraySchema,
-    productImageArrayRequestSchema,
-    productImageArrayResponseSchema
-
+    productImageArrayResponseSchema,
+    productImageArrayRequestSchema
 };

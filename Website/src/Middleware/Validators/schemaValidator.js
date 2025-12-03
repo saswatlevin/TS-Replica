e5127031ca1ae6dccefe993fc11e424880efd5f3
Saswat K. Levin <saswatlevin@gmail.com>
2025-12-04 01:31:59 +0300

    Removed unnecessary display logic in schemaValidator.js

--- Website/src/Middleware/Validators/schemaValidator.js
+++ Website/src/Middleware/Validators/schemaValidator.js
@@ -4,17 +4,16 @@ const schemaValidator = (schema, object) => {
     console.log("In schemaValidator");
     //console.log("##DEBUG - object ", object);
     const result = schema.parse(object);
+    //console.log("schema validation errors - ", result?.error?.errors);
     
     
-    
-    if (!result.success){
+    /*if (!result.success){
         console.log("schema validation errors - ", result?.error?.errors);
     }
 
     else {
-        ;
         console.log("schema validation result ", result);
-    }
+    }*/
     
     return result;
     

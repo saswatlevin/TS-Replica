38f4aae114e79104b098f78cc6ef2ff71cd97a95
Saswat K. Levin <saswatlevin@gmail.com>
2025-12-12 13:13:12 +0300

    Added a route for updateCartItemQuantity in cartItemRoures.js

--- Website/src/Middleware/Routes/cartItemRoutes.js
+++ Website/src/Middleware/Routes/cartItemRoutes.js
@@ -6,5 +6,6 @@ const cartItemSchemas = require('../Schemas/cartItemSchemas');
 const { xss } = require('express-xss-sanitizer');
 
 router.route('/createcartitem/:user_id/:product_id').post(requestValidator(cartItemSchemas.cartItemRequestSchema), xss(), cartItemControllers.createCartItem);
+router.route('/updatecartitemquantity/:user_id/:product_id').patch(requestValidator(cartItemSchemas.updateCartItemQuantitySchema), xss(), cartItemControllers.updateCartItemQuantity);
 
 module.exports = router;

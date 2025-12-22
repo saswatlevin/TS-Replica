943ba8abaad94c263d0110c01aa526240787ab46
Saswat K. Levin <saswatlevin@gmail.com>
2025-12-22 13:18:36 +0300

    Removed the product_id from the routes of createCartItem() and updateCartItemQuantity() in cartItemRoutes,js

--- Website/src/Middleware/Routes/cartItemRoutes.js
+++ Website/src/Middleware/Routes/cartItemRoutes.js
@@ -5,7 +5,8 @@ const requestValidator = require('../Validators/requestValidator');
 const cartItemSchemas = require('../Schemas/cartItemSchemas');
 const { xss } = require('express-xss-sanitizer');
 
-router.route('/createcartitem/:user_id/:product_id').post(requestValidator(cartItemSchemas.cartItemRequestSchema), xss(), cartItemControllers.createCartItem);
-router.route('/updatecartitemquantity/:user_id/:product_id').patch(requestValidator(cartItemSchemas.updateCartItemQuantitySchema), xss(), cartItemControllers.updateCartItemQuantity);
+router.route('/createcartitem/:user_id').post(requestValidator(cartItemSchemas.cartItemRequestSchema), xss(), cartItemControllers.createCartItem);
+
+router.route('/updatecartitemquantity/:user_id').patch(requestValidator(cartItemSchemas.updateCartItemQuantitySchema), xss(), cartItemControllers.updateCartItemQuantity);
 
 module.exports = router;

e148c599196ba0953eb2a8134185e3330d2dc649
Saswat K. Levin <saswatlevin@gmail.com>
2025-12-24 17:06:56 +0300

    Made it possible for the checkCartItemExists() support function to search for cart items with or without cart_item_id.

--- Website/src/Middleware/Controllers/SupportFunctions/cartItemSupportFunctions.js
+++ Website/src/Middleware/Controllers/SupportFunctions/cartItemSupportFunctions.js
@@ -1,29 +1,48 @@
 const mongoose = require('mongoose');
 const User = require('../../Models/User');
 
-const checkCartItemExists = async(req) => {
+const checkCartItemExists = async(req, cart_item_id = null) => {
     console.log("In checkCartItemExists");
     
     // USES REQ.BODY -> PRODUCT_ID
     try {
         const product_id = req.body.product_id;
 
+        const sku = req.body.sku;
+
         //console.log("##DEBUG - product_id in checkCartItemExists ", product_id);
 
-        const cart_item_query = {
-            "CartItems.product_id": product_id
-        };
+        const cart_item_query = {};
 
+        if (cart_item_id !== null) {
+            
+            cart_item_query = {
+                "CartItems.product_id": product_id,
+                "CartItems.sku": sku
+            }
+        }
+
+        else {
+
+            cart_item_query = {
+                "CartItems.cart_item_id": cart_item_id,
+                "CartItems.product_id": product_id,
+                "CartItems.sku": sku    
+            }
+        }
+
+        console.log("##DEBUG - cart_item_query in checkCartItemExists() is ", cart_item_query);
+        
         const result = await User.findOne(cart_item_query);
-
-        //console.log("##DEBUG - Result in checkCartItemExists - ", result);
+        
+        console.log("##DEBUG - Result in checkCartItemExists ", result);
 
         if (result === null) {
-            //console.log("##DEBUG - In checkCartItemExists - returning false");
+            console.log("##DEBUG - In checkCartItemExists - returning false");
             return false;
         }
 
-        //console.log("##DEBUG - In checkCartItemExists - returning true");
+        console.log("##DEBUG - In checkCartItemExists - returning true");
 
         return true;
     }
@@ -33,7 +52,6 @@ const checkCartItemExists = async(req) => {
         throw error;
     }
 };
-
 
 
 module.exports = {

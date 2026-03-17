b44bed828b8c3bf5ab988f3a3a7adde7c6e954ae
Saswat Levin <saswatlevin@gmail.com>
2026-03-18 01:56:15 +0530

    Updated the checkDuplicateProductImageExists and checkProductImageExists functions and added the getProductImageArrayLength function productImageSupportFunctions.js

--- Website/src/Middleware/Controllers/SupportFunctions/productImageSupportFunctions.js
+++ Website/src/Middleware/Controllers/SupportFunctions/productImageSupportFunctions.js
@@ -1,58 +1,94 @@
 const Product = require('../../Models/Product');
 
 const checkDuplicateProductImageExists = async(req) => {
-     console.log("In checkDuplicateProductImageExists");
+     console.log("In checkDuplicateProductImageExists (HELPER FUNCTION)");
 
      try{
-        const product_id = req.params.product_id;
+        const product_id = req.body.product_id;
         const image_uri = req.body.image_uri;
 
-        const product_image_query = {"ProductImages.image_uri": image_uri};
+        const product_image_query = {product_id: product_id, "product_images.image_uri": image_uri};
 
         const result = await Product.findOne(product_image_query);
 
+        //console.log("##DEBUG - result in checkDuplicateProductImageExists ", result);
+
         if(result === null){
+            console.log("##DEBUG - checkDuplicateProductImageExists returns false");
             return false;
         }
 
         else{
+            console.log("##DEBUG - checkDuplicateProductImageExists returns true");
             return true;
         }
      }
 
      catch(error){
         console.log("Error in checkDuplicateProductImageExists ", error);
-        return error;
+        throw error;
      }
 };
 
 const checkProductImageExists = async(req) => {
-    console.log("In checkImageExists");
+    console.log("In checkProductImageExists (HELPER FUNCTION)");
 
     try{
+
+        const product_id = req.body.product_id;
+
         const image_id = req.body.image_id;
 
-        const product_image_query = {"ProductImages.image_id": image_id};
+        const product_image_query = {product_id: product_id, "product_images.image_id": image_id};
 
         const result = await Product.findOne(product_image_query);
 
         if(result === null){
+            console.log("##DEBUG - checkProductImageExists returns false");
             return false;
         }
 
         else{
+            console.log("##DEBUG - checkProductImageExists returns true");
             return true;
         }
      }
 
      catch(error){
         console.log("Error in checkProductImageExists ", error);
-        return error;
+        throw error;
      }
 
-}
+};
+
+const getProductImageArrayLength = async(req) => {
+    console.log("In getProductImageArrayLength (HELPER FUNCTION)");
+
+    try{
+
+        const product_id = req.body.product_id;
+
+        const image_id = req.body.image_id;
+
+        const product_image_query = {product_id: product_id};
+
+        const result = await Product.findOne(product_image_query);
+
+        const product_image_subdocument_array_length = result.product_images.length;
+        
+        console.log("Length of the Product Images subdocument array ", product_image_subdocument_array_length);
+
+        return product_image_subdocument_array_length;
+    }
+
+    catch(error) {
+        console.log("Error in getProductImageArrayLength ", error);
+        throw error;
+    }
+};
 
 module.exports = { 
     checkDuplicateProductImageExists, 
-    checkProductImageExists 
+    checkProductImageExists,
+    getProductImageArrayLength
 }; 

e5b5ad60a0aa869155c7b930ffd32233836000d5
Saswat Levin <saswatlevin@gmail.com>
2026-03-18 16:19:48 +0530

    Added a schema debug statement to schemaValidator.js

--- Website/src/Middleware/Validators/schemaValidator.js
+++ Website/src/Middleware/Validators/schemaValidator.js
@@ -2,6 +2,7 @@ const z = require('zod');
 
 const schemaValidator = (schema, object) => {
     console.log("In schemaValidator");
+    //console.log("##DEBUG - schema ", schema);
     //console.log("##DEBUG - object ", object);
     const result = schema.parse(object);
     //console.log("schema validation errors - ", result?.error?.errors);

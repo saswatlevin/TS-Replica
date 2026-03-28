844c6b7ab7ccfa071903a5d9b1660429f70a59fc
Saswat Levin <saswatlevin@gmail.com>
2026-03-29 02:41:27 +0530

    Corrected source of product_id and product item query for checkProductItemExists(), added ?. operators to if conditions in checkDuplicateProductItemExists() and created checkProductItemValueExists() in productItemSupportFunctions.js

--- Website/src/Middleware/Controllers/SupportFunctions/productItemSupportFunctions.js
+++ Website/src/Middleware/Controllers/SupportFunctions/productItemSupportFunctions.js
@@ -1,85 +1,136 @@
 const Product = require('../../Models/Product');
+const pruneObject = require('../../pruneObject');
+const _ = require('lodash');
 
 const checkProductItemExists = async(req) => {
-    console.log("In checkProductItemExists");
+    console.log("In checkProductItemExists (HELPER FUNCTION)");
 
     try{
-        const product_id = req.params.product_id;
+        const product_id = req.body.product_id;
         const sku = req.body.sku;
 
 
-        const product_item_query = {"ProductItems.sku": sku};
+        const product_item_query = {product_id: product_id, "product_items.sku": sku};
+        //console.log("product_item_query in checkProductItemExists ", product_item_query);
 
         const result = await Product.findOne(product_item_query);
 
-        if (result === null){
+        if (result === null) {
+            console.log("##DEBUG - checkProductItemExists returns false");
             return false;
         }
 
-        else{
+        else {
+            console.log("##DEBUG - checkProductItemExists returns true");
             return true;
         }
     }
 
     catch(error){
         console.log("Error in checkProductItemExists ", error);
-        return error;
+        throw error;
     }
 
 };
 
 const checkDuplicateProductItemExists = async(req) => {
-    console.log("In checkDuplicateProductItemExists");
+    console.log("In checkDuplicateProductItemExists (HELPER FUNCTION)");
 
     try{
         var product_item_query = {};
 
+        const product_id = req.body.product_id;
+
         const request_body = req.body;
         
-        if (request_body.upper_size_number !== undefined && request_body.upper_size_letter !== undefined){
-            product_item_query = {"ProductItems.upper_size_number": request_body.upper_size_number, "ProductItems.upper_size_letter": request_body.upper_size_letter};
+        if (request_body?.upper_size_number !== undefined && request_body?.upper_size_letter !== undefined){
+            product_item_query = {product_id: product_id, "product_items.upper_size_number": request_body.upper_size_number, "product_items.upper_size_letter": request_body.upper_size_letter};
         
         }
 
-        else if (request_body.lower_size_number !== undefined && request_body.inseam_length !== undefined){
-            product_item_query = {"ProductItems.lower_size_number": request_body.lower_size_number, "ProductItems.inseam_length": request_body.inseam_length};
+        else if (request_body?.lower_size_number !== undefined && request_body?.inseam_length !== undefined){
+            product_item_query = {product_id: product_id, "product_items.lower_size_number": request_body.lower_size_number, "product_items.inseam_length": request_body.inseam_length};
         }
 
-        else if (request_body.lower_size_letter !== undefined && request_body.inseam_length !== undefined){
-            product_item_query = {"ProductItems.lower_size_letter": request_body.lower_size_letter, "ProductItems.inseam_length": request_body.inseam_length};
-        }
-
-       else if (request_body.lower_size_letter !== undefined){
-            product_item_query = {"ProductItems.lower_size_letter": request_body.lower_size_letter};
+        else if (request_body?.lower_size_letter !== undefined && request_body?.inseam_length !== undefined){
+            product_item_query = {product_id: product_id, "product_items.lower_size_letter": request_body.lower_size_letter, "product_items.inseam_length": request_body.inseam_length};
        }
 
-       else if (request_body.lower_size_number !== undefined){
-            product_item_query = {"ProductItems.lower_size_number": request_body.lower_size_number};
+       else if (request_body?.lower_size_letter !== undefined){
+            product_item_query = {product_id: product_id, "product_items.lower_size_letter": request_body.lower_size_letter};
+       }
+
+       else if (request_body?.lower_size_number !== undefined){
+            product_item_query = {product_id: product_id, "product_items.lower_size_number": request_body.lower_size_number};
        }
 
        else {
         throw new Error("Invalid request body in checkDuplicateProductItemExists.");
        }
 
+        //console.log("product_item_query in checkDuplicateProductItemExists ", product_item_query);
         const result = await Product.findOne(product_item_query);
+        //console.log("##DEBUG - result in checkDuplicateProductItemExists ", result);
 
         if (result === null){
+            console.log("##DEBUG - checkDuplicateProductItemExists returns false");
             return false;
         }
 
         else{
+            console.log("##DEBUG - checkDuplicateProductItemExists returns true");
             return true;
         }
     }
 
     catch(error){
         console.log("Error in checkProductItemExists ", error);
-        return error;
+        throw error;
     }
 
 };
 
+const checkProductItemValueExists = async(req) => {
+    console.log("In checkProductItemValueExists (HELPER FUNCTION)");
+
+    try {
+
+        const product_id = req.body.product_id;
+        
+        const request_body_deep_clone = _.cloneDeep(req.body);
+
+        const product_item_search_query = {};
+
+        for (const [key, value] of Object.entries(request_body_deep_clone)) {
+            
+            if (key !== "product_id") {
+                product_item_search_query[`product_items.${key}`] = value;
+            }
+        }
+
+        //console.log("##DEBUG - product_item_search_query in checkProductItemValueExists - ", product_item_search_query);
+
+        const result = await Product.findOne(product_item_search_query);
+
+        if (result === null) {
+            console.log("##DEBUG - checkProductItemValueExists returns false");
+            return false;
+        }
+
+        else {
+            console.log("##DEBUG - checkProductItemValueExists returns true");
+            return true;
+        }
+    }
+    
+    catch(error) {
+        console.log("Error in checkProductItemValueExists ", error);
+        throw error;
+    }
+};
+
 module.exports = { 
     checkProductItemExists, 
-    checkDuplicateProductItemExists 
+    checkDuplicateProductItemExists,
+    checkProductItemValueExists
 };

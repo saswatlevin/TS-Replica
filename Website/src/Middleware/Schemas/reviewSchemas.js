2eade4ebe12be76b80086f93e8526c5e23efd36b
Saswat Levin <saswatlevin@gmail.com>
2026-04-03 10:50:03 +0530

    Added the createReviewRequestSchema, updateReviewRequestSchema, getReviewByIdRequestSchema, deleteReviewRequestSchema, searchReviewRequestSchema, createReviewResponseSchema and searchReviewResponseSchema to reviewSchemas.js

--- Website/src/Middleware/Schemas/reviewSchemas.js
+++ Website/src/Middleware/Schemas/reviewSchemas.js
@@ -1,49 +1,94 @@
 const z = require('zod');
 const customValidators = require('../Validators/CustomValidators/customFormatValidators');
 const objectIdSchema = require('./objectIdSchema');
-const { testReviewObject, testReviewObject2} = require('./TestObjects/testReviewObject');
+const productValidators = require('../Validators/CustomValidators/productValidators');
+const reviewValidators = require('../Validators/CustomValidators/reviewValidators');
 
-const reviewRequestSchema = z.object({
-    review_id: z.string("The review_id field must be a string. It is a required field.").length(12, {message: "The review_id field must be a string that is 12 characters long"}).regex(customValidators.twelveCharacterRegex, {message: "The review_id can only contain lowercase letters and digits."}),
+const createReviewRequestSchema = z.object({
+    
+    product_id: productValidators.zodIsProductId,
+
+    review_comment: reviewValidators.zodIsReviewComment,
+
+    review_rating: reviewValidators.zodIsReviewRating
+}).strict();
+
+const updateReviewRequestSchema = z.object({
+    review_id: reviewValidators.zodIsReviewId,
+
+    product_id: productValidators.zodIsProductId,
+
+    review_comment: reviewValidators.zodIsReviewComment.optional(),
+
+    review_rating: reviewValidators.zodIsReviewRating.optional()
+}).strict();
+
+const getReviewByIdRequestSchema = z.object({
+    review_id: reviewValidators.zodIsReviewId
+}).strict();
+
+const deleteReviewRequestSchema = z.object({
+    product_id: productValidators.zodIsProductId,
+    review_id: reviewValidators.zodIsReviewId
+}).strict();
+
+const createReviewResponseSchema = z.object({
+    review_id: reviewValidators.zodIsReviewId,
 
     user_id: z.string("The user_id field must be a string. It is a required field.").length(12, {message: "The user_id must be 12 characters long."}).regex(customValidators.twelveCharacterRegex, {message: "The user_id can only contain lowercase letters and numbers."}),
 
-    product_id: z.string("The product_id field must be a string. It is a required field.").length(12, {message: "The product_id must be 12 characters long."}).regex(customValidators.twelveCharacterRegex, {message: "The product_id can only contain lowercase letters and numbers."}),
+    product_id: productValidators.zodIsProductId,
 
-    docType: z.literal("REVIEW", {message: "The docType field is a required field. It takes the value: REVIEW."}),
+    docType: reviewValidators.zodIsReviewDocType,
 
-    review_comment: z.string("The review_comment field must be a string.").min(3, {message: "The review_comment field has a minimum limit of 50 characters. It is a required field."}).max(500, {message: "The review_comment field has a maximum limit of 500 characters."}).regex(customValidators.paragraphRegex, {message: "The review_comment field accepts only uppercase letters, lowercase letters, uppercase accented letters, lowercase accented letters, digits, hashes, exclamation marks, commas, semicolons, colons, percentage symbols, single-quotes, typeset-single-quotes, double-quotes, typeset-double-quotes, backward-slash, brackets, hyphens, long hyphens, dots, and spaces."}),
+    review_comment: reviewValidators.zodIsReviewComment,
 
-    review_rating: z.number("The review_rating field must be a number (integer or floating-point).").min(1, {message: "The review_rating field has a minimum limit of 1."}).max(5, {message: "The review_rating field has a maximum limit of 5."})
-});
+    review_rating: reviewValidators.zodIsReviewRating,
+
+    _id: objectIdSchema,
+
+    __v: customValidators.zodIsDocumentVersion
+
+}).strict();
 
 const reviewResponseSchema = z.object({
     _id: objectIdSchema,
 
-    review_id: z.string("The review_id field must be a string. It is a required field.").length(12, {message: "The review_id field must be a string that is 12 characters long"}).regex(customValidators.twelveCharacterRegex, {message: "The review_id can only contain lowercase letters and digits."}),
+    review_id: reviewValidators.zodIsReviewId,
 
     user_id: z.string("The user_id field must be a string. It is a required field.").length(12, {message: "The user_id must be 12 characters long."}).regex(customValidators.twelveCharacterRegex, {message: "The user_id can only contain lowercase letters and numbers."}),
 
-    product_id: z.string("The product_id field must be a string. It is a required field.").length(12, {message: "The product_id must be 12 characters long."}).regex(customValidators.twelveCharacterRegex, {message: "The product_id can only contain lowercase letters and numbers."}),
+    product_id: productValidators.zodIsProductId,
 
-    docType: z.literal("REVIEW", {message: "The docType field is a required field. It takes the value: REVIEW."}),
+    docType: reviewValidators.zodIsReviewDocType,
 
-    review_comment: z.string("The review_comment field must be a string.").min(3, {message: "The review_comment field has a minimum limit of 50 characters. It is a required field."}).max(500, {message: "The review_comment field has a maximum limit of 500 characters."}).regex(customValidators.paragraphRegex, {message: "The review_comment field accepts only uppercase letters, lowercase letters, uppercase accented letters, lowercase accented letters, digits, hashes, exclamation marks, commas, semicolons, colons, percentage symbols, single-quotes, typeset-single-quotes, double-quotes, typeset-double-quotes, backward-slash, brackets, hyphens, long hyphens, dots, and spaces."}),
+    review_comment: reviewValidators.zodIsReviewComment,
 
-    review_rating: z.number("The review_rating field must be a number (integer or floating-point).").min(1, {message: "The review_rating field has a minimum limit of 1."}).max(5, {message: "The review_rating field has a maximum limit of 5."})
-});
+    review_rating: reviewValidators.zodIsReviewRating,
+
+    __v: customValidators.zodIsDocumentVersion
+
+}).strict();
+
+const searchReviewRequestSchema = z.object({
+    product_id: productValidators.zodIsProductId.optional(),
+
+    review_comment: reviewValidators.zodIsReviewComment.optional(),
+
+    review_rating: reviewValidators.zodIsReviewRating.optional()
+}).strict();
+
+const searchReviewResponseSchema = z.array(reviewResponseSchema);
+
 
 module.exports = {
-    reviewRequestSchema,
+    createReviewRequestSchema,
+    updateReviewRequestSchema,
+    getReviewByIdRequestSchema,
+    deleteReviewRequestSchema,
     reviewResponseSchema,
+    searchReviewRequestSchema,
+    createReviewResponseSchema,
+    searchReviewResponseSchema
 };
-
-// TESTS
-const reviewResult = reviewRequestSchema.safeParse(testReviewObject);
-console.log("reviewResult ", reviewResult);
-console.log("reviewResult?.error?.issues ", reviewResult?.error?.issues);
-
-const reviewResult2 = reviewResponseSchema.safeParse(testReviewObject2);
-console.log("reviewResult2 ", reviewResult2);
-console.log("reviewResult2?.error?.issues ", reviewResult2?.error?.issues);
 

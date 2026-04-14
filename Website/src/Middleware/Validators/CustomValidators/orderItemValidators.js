aa27490a5ad5b37722c9dd96327be66f029dbe91
Saswat Levin <saswatlevin@gmail.com>
2026-04-14 15:01:04 +0530

    Created validators for all the orderItem fields in orderItemValidators.js

---
+++ Website/src/Middleware/Validators/CustomValidators/orderItemValidators.js
@@ -0,0 +1,26 @@
+const z = require('zod');
+
+const zodIsOrderItemId = z.string("The order_item_id field must be a string. It is a required field.").length(12, {message: "The order_item_id must be 12 characters long."}).regex(customValidators.twelveCharacterRegex, {message: "The order_item_id can contain only lowercase letters and digits."});
+
+const zodIsOrderItemName = z.string("The order_item_name field must be a string.").min(3, {message: "The order_item_name field is a required field."}).max(100, {message: "The maximum permitted length is 100 characters."}).regex(customValidators.productNameRegex, {message: "The order_item_name field accepts only upper case letters, lower case letters, accented uppercase and lowercase letters and spaces."});
+
+const zodIsOrderItemPrice = z.number("The order_item_price field must be a number(integer). It is a required field.").int("The order_item_price field must be an integer.").min(1, {message: "The minimum limit of the order_item_price field is 1."}).max(300, {message: "The maximum limit of the order_item_price field is 300."});
+
+const zodIsOrderItemQuantity = z.number("The order_item_quantity field must be a number(integer). It is a required field.").int("The order_item_quantity field must be an integer.").min(1, {message: "The order_item_quantity field has a minimum limit of 1."}).max(100, {message: "The order_item_quantity field has a maximum upper limit of 100."});
+
+const zodIsOrderItemImageURI = z.string("The order_item_image_uri must be a string. It is a required field.").max(256).regex(customValidators.windowsFilePathRegex, {message: "The order_item_image_uri must be a valid Windows file path. Maximum permitted length is 256 characters."});
+
+const zodIsReturnReason = z.enum(["None", "Defect", "Cancelled"], {message: "The return_reason field is a required field. It must be one of the following values: None, Defect, Cancelled."});
+
+const zodIsReturnStatus = .enum(["None", "Requested", "Approved", "Cancelled"], {message: "The return_status field is a required field. It must be one of the following values: None, Defect, Cancelled."});
+
+module.exports = {
+	zodIsOrderItemId,
+	zodIsorderItemName,
+	zodIsOrderItemPrice,
+	zodIsOrderItemQuantity,
+	zodIsReturnStatus
+}
+
+
+

7bbc435cd45f737f5a0ae5b61edcdf330a78950e
Saswat Levin <saswatlevin@gmail.com>
2026-04-23 16:02:47 +0530

    Changed all the discount validators from int to number in discountValidators.js.

--- Website/src/Middleware/Validators/CustomValidators/discountValidators.js
+++ Website/src/Middleware/Validators/CustomValidators/discountValidators.js
@@ -2,18 +2,20 @@ const z = require('zod');
 
 const zodIsDiscountCode = z.enum(["None", "10PERCENT", "20PERCENT", "30PERCENT", "40PERCENT", "50PERCENT"], {message: "The discount_code field accepts one of the following values: None, 10PERCENT, 20PERCENT, 30PERCENT, 40PERCENT, 50PERCENT. It is a required field."});
 
-const zodIsDiscountPercentage = z.number("The discount_percentage field must be a number (integer).").int("The discount_percentage field must be an integer value.").min(0, {message: "The discount_percentage field only takes integer values. Its minimum limit is 0."}).max(100, {message: "The maximum limit of the discount_percentage field is 100."});
+const zodIsDiscountPercentage = z.number({invalid_type_error: "The discount_percentage field must be a number."
+}).min(0, { message: "The discount_percentage field has a minimum limit of 0." }).max(100, { message: "The discount_percentage has a maximum limit of 100."});
 
-const zodIsDiscountAmount = z.number("The discount_amount field must be a number (integer).").int("The discount_amount field must be an integer value.").min(0, {message: "The discount_amount field only takes integer values. Its minimum limit is 0."}).max(300, {message: "The maximum limit of the discount_amount field is 300."});
-    
-const zodIsDiscountedTotal = z.number("The discounted_total field must be a number (integer).").int("The discounted_total field must be an integer value.").min(0, {message: "The discounted_total field only takes integer values. Its minimum limit is 0."}).max(10000, {message: "The maximum limit of the discounted_total field is 10000."});
+const zodIsDiscountAmount = z.number({invalid_type_error: "The discount_amount field must be a number."
+}).min(0, { message: "The discount_amount field has a minimum limit of 0."}).max(300, { message: "The discount_amount field has a maximum limit of 300."});
 
-const zodIsTotalDiscountAmount = z.number("The total_discount_amount field must be a number (integer).").int("The total_discount_amount must be an integer value.").min(0, {message: "The total_discount_amount field only takes integer values. Its minimum limit is 0."}).max(10000, {message: "The maximum limit of the discounted_total field is 10000."});
+const zodIsDiscountedTotal = z.number({invalid_type_error: "The discounted_total field must be a number."
+}).min(0, { message: "The discounted_total field has a minimum limit of 0."}).max(10000, { message: "The discounted_total field has a maximum limit of 10000."});
 
-const zodIsTotalDiscountPercentage = z.number("The total_discount_percentage field must be a number (integer).").int("The total_discount_percentage field must be an integer value.").min(0, {message: "The total_discount_percentage field has a minimum limit of 0"}).max(100, {message: "The total_discount_percentage value has a maximum limit of 100"});
+const zodIsTotalDiscountAmount = z.number({invalid_type_error: "The total_discount_amount must be a number."}).min(0, { message: "The total_discount_amount field has a minimum limit of 0."}).max(10000, { message: "The total_discount_amount has a maximum limit of 10000."});
 
-const zodIsTotalDiscountedTotal = z.number("The total_discounted_total field must be a number (integer).").int("The total_discounted_total field must be an integer value.").min(0, {message: "The total_discounted_total field only takes integer values. Its minimum limit is 0."}).max(10000, {message: "The maximum limit of the total_discounted_total field is 10000."});
+const zodIsTotalDiscountPercentage = z.number({invalid_type_error: "The total_discount_percentage must be a number."}).min(0, { message: "The total_discount_percentage field has a minimum limit of 0."}).max(100, { message: "The total_discount_percentage field has a maximum limit of 100."});
 
+const zodIsTotalDiscountedTotal = z.number({invalid_type_error: "The total_discounted_total must be a number."}).min(0, { message: "The total_discounted_total field has a minimum limit of 0."}).max(10000, { message: "The total_discounted_total has a maximum limit of 10000."});
 
 module.exports = {
 	zodIsDiscountCode,

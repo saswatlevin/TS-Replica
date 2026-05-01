c53faf5c299adcfcbbba14557ca1637f1d4e68b6
Saswat Levin <saswatlevin@gmail.com>
2026-05-01 20:01:03 +0530

    Added checkDuplicateShippingAddress() to createShippingAddress() and created variables for the filter and update object. Created a filter and pruned the shipping_address_id from the update object in updateShippingAddresssId. Replaced the aggregation pipeline with findOne() and find() respectively in getShippingAddressById and searchShippingAddress. Added checkShippingAddressExists(), a filter variable and query variable in deleteShippingAddressById All in shippingAddressControllers.js

--- Website/src/Middleware/Controllers/shippingAddressControllers.js
+++ Website/src/Middleware/Controllers/shippingAddressControllers.js
@@ -27,6 +27,13 @@ const createShippingAddress = asyncErrorHandler(async (req, res, next) => {
         throw error;
     }
 
+    console.log("Checking if the shipping address already exists");
+    if (await checkDuplicateShippingAddressExists(req) === true) {
+        const duplicate_subdocument_error = new DuplicateSubDocumentError(`Could not create the shipping_address for the user with user_id ${user_id} since it already exists.`);
+
+        throw duplicate_subdocument_error; 
+    }
+
     // Deep clone the request body
     const request_body_deep_clone = JSON.parse(JSON.stringify(req.body));
     console.log("request_body_deep_clone ", request_body_deep_clone);
@@ -43,10 +50,16 @@ const createShippingAddress = asyncErrorHandler(async (req, res, next) => {
     
     console.log("shipping_address ", shipping_address);
 
+    const filter = {user_id: user_id};
+    console.log("filter ", filter);
+
+    const shipping_address_object = { $push: { ShippingAddresses: shipping_address } };
+    console.log("shipping_address_object ", shipping_address_object);
+
     // A live mongoose document contains many hiiden keys and fields. 
     // The .lean() functions strips these keys and fields completely.
-    const result = await User.findOneAndUpdate({ user_id: user_id }, { $push: { ShippingAddresses: shipping_address } }, { new: true }, {runValidators: true}).lean(); // return the updated document;
-    console.log("result ", result);
+    const result = await User.findOneAndUpdate(filter, shipping_address_object, { new: true }, {runValidators: true}).lean(); // return the updated document;
+    console.log("result in createShippingAddress ", result);
     
     // Here, we use 201 since it means that the resource has been created.
     res.status(201).json(result);
@@ -64,7 +77,6 @@ const updateShippingAddress = asyncErrorHandler(async (req, res, next) => {
     const shipping_address_id = req.body.shipping_address_id;
     console.log("shipping_address_id ", shipping_address_id);
 
-
     console.log("Checking if the request body is empty or not");
     if (checkIsEmptyObject(req) === true) {
         const empty_request_body_error = new EmptyRequestBodyError(`Could not update shipping address with shipping_address_id ${shipping_address_id} for user with user_id ${user_id} as the request body is empty.`);
@@ -80,53 +92,33 @@ const updateShippingAddress = asyncErrorHandler(async (req, res, next) => {
 
     console.log("Check whether the shipping address exists or not");
     if (await checkShippingAddressExists(req) === false) {
-        const shipping_address_not_found_error = new ResourceNotFoundError(`Could not update the shipping address with ${shipping_address_id} since it does not exist.`);
+        const shipping_address_not_found_error = new ResourceNotFoundError(`Could not update the shipping address with shipping_address_id ${shipping_address_id} since it does not exist.`);
         throw shipping_address_not_found_error;
     }
 
     // The user_id of the User document where the shipping address is to be updated.
     // The shipping_address_id of the shipping_address to be updated.
-    const shipping_address_selector = { user_id: user_id, "ShippingAddresses.shipping_address_id": shipping_address_id };
-    console.log("shipping_address_selector ", shipping_address_selector);
+    const filter = { user_id: user_id, "ShippingAddresses.shipping_address_id": shipping_address_id };
+    console.log("filter ", filter);
 
     const request_body_deep_clone = JSON.parse(JSON.stringify(req.body));
     console.log("request_body_deep_clone ", request_body_deep_clone);
 
     // Building the shipping address update request body.
-    const update_request = Object.fromEntries(
+    const update_object = Object.fromEntries(
         Object.entries(request_body_deep_clone).map(([key, value]) => [`ShippingAddresses.$.${key}`, value])
     );
 
-    console.log("update_request ", update_request);
+    const final_update_object = pruneObject(update_object, ['shipping_address_id']);
+    console.log("final_update_object ", final_update_object);
 
-    // Returns some stats regardless of successful update or not.
-    // A live mongoose document contains many hiiden keys and fields. 
-    // The .lean() functions strips these keys and fields completely.
-    const result = await User.updateOne(
-        shipping_address_selector,
-
-        {
-            $set: update_request
-        }
-    ).lean();
+    const result = await User.findOneAndUpdate(filter, { $set: final_update_object }, {new: true}, {runValidators: true} ).lean();
     
-    console.log("result ", result);
+    console.log("result in updateShippingAddress ", result);
 
-    if (result["modifiedCount"] === 1 && result["matchedCount"] === 1) {
-        // OK
-        // Here, 200 is used since the server successfully updates the resource and returns the updated representation in the response body
-        res.status(200).json(result);
-    }
-
-    else {
-        // No Change
-        // Here, 200 is used since there's no change to the given resource.
-        res.status(200).json(result);
-    }
+    res.status(200).json(result);
 
     console.log("===END OF updateShippingAddress===");
-
-
 });
 
 const getShippingAddressById = asyncErrorHandler(async (req, res, next) => {
@@ -151,53 +143,14 @@ const getShippingAddressById = asyncErrorHandler(async (req, res, next) => {
     const shipping_address_id = req.body.shipping_address_id;
     console.log("shipping_address_id ", shipping_address_id);
 
-    // Aggregation pipeline to get the shipping_address sub-document
-    /**
-     * Here, the first $match operator matches the User document where we want to search addresses.
-     * Here, the $unwind operator splits the ShippingAddresses array into individual sub-documents (array elements).
-     * The 2nd $match operator then matches the sub-document with the given shipping_address_id.
-     * The $project operator then extracts the given fields from the array elements.  
-     */
-    const shipping_address_aggregation_pipeline = [
-        { $match: { user_id: user_id } },
+    const search_query = { user_id: user_id, "ShippingAddresses.shipping_address_id": shipping_address_id };
 
-        { $unwind: "$ShippingAddresses" },
+    const projection_object = { _id: 0, ShippingAddresses: 1 };
 
-        { $match: { "ShippingAddresses.shipping_address_id": shipping_address_id } },
-        {
-            $project:
-            {   _id: 0,
-                shipping_address_id: "$ShippingAddresses.shipping_address_id",
-                address_type_id: "$ShippingAddresses.address_type_id",
-                company_name: "$ShippingAddresses.company_name",
-                address: "$ShippingAddresses.address",
-                apartment: "$ShippingAddresses.apartment",
-                city: "$ShippingAddresses.city",
-                administrative_division: "$ShippingAddresses.city",
-                country: "$ShippingAddresses.country",
-                postal_area: "$ShippingAddresses.postal_area",
-                phone_number: "$ShippingAddresses.phone_number"
-            }
-        }
-    ];
-
-    // Returns [] if resource not found
-    const result = await User.aggregate(shipping_address_aggregation_pipeline);
-    console.log("result ", result[0]);
+    const result = await User.findOne(search_query, projection_object).lean();
+    console.log("result in getShippingAddressById ", result);
     
-
-    if (result.length === 1) {
-        // Resource Found
-        // Return the shipping address in the Shipping Addresses array.
-        // Here, 200 is used since the resource is found and successfully returned at the requested URL (no redirect).
-        //console.log("result[0] ", result[0]);
-        res.status(200).json(result[0]);
-    }
-
-    else {
-        const shipping_address_not_found_error = new ResourceNotFoundError(`Could not find the shipping address with ${shipping_address_id} since it does not exist.`);
-        throw shipping_address_not_found_error;
-    }
+    res.status(200).json(result);
 
     console.log("===END OF getShippingAddressById===");
 
@@ -208,12 +161,13 @@ const searchShippingAddress = asyncErrorHandler(async (req, res, next) => {
 
     const user_id = req.params.user_id;
 
-    // HERE, WE CHECK IF THE REQUEST BODY IN PARTICULAR IS EMPTY OR NOT
+    console.log("Checking if the request body is empty");
     if (checkIsEmptyObject(req) === true) {
         const empty_request_body_error = new EmptyRequestBodyError(`Could not find the shipping address of user with user_id ${user_id} due to empty request body.`);
         throw empty_request_body_error;
     }
 
+    console.log("Checking if the user exists");
     if (await checkUserExists(req) === false) {
         const user_id_not_found_error = new ResourceNotFoundError(`Could not find the shipping address since the user with user_id ${user_id} does not exist.`);
         throw user_id_not_found_error;
@@ -224,55 +178,20 @@ const searchShippingAddress = asyncErrorHandler(async (req, res, next) => {
 
     console.log("request_body in search_shipping_address is ", request_body_deep_clone);
 
-
-    // Building the shipping address search query
-    const shipping_address_search_query = Object.fromEntries(
+    // Building the preliminary query
+    const preliminary_query = Object.fromEntries(
         Object.entries(request_body_deep_clone).map(([key, value]) => [`ShippingAddresses.${key}`, value])
     );
 
+    const shipping_address_search_query = {user_id: user_id, ...preliminary_query};
+
+    const projection_object = { _id: 0, ShippingAddresses: 1 };
 
     console.log("shipping_address_search_query ", shipping_address_search_query);
 
-    const shipping_address_aggregation_pipeline = [
-        { $match: { user_id: user_id } },
+    const result = User.find(shipping_address_search_query, projection_object).lean();
 
-        { $unwind: "$ShippingAddresses" },
-
-        { $match: shipping_address_search_query },
-        {
-            $project: {
-                _id: 0,
-                shipping_address_id: "$ShippingAddresses.shipping_address_id",
-                address_type_id: "$ShippingAddresses.address_type_id",
-                company_name: "$ShippingAddresses.company_name",
-                address: "$ShippingAddresses.address",
-                apartment: "$ShippingAddresses.apartment",
-                city: "$ShippingAddresses.city",
-                administrative_division: "$ShippingAddresses.city",
-                country: "$ShippingAddresses.country",
-                postal_area: "$ShippingAddresses.postal_area",
-                phone_number: "$ShippingAddresses.phone_number"
-            }
-        }
-    ];
-
-    // Returns [] if resource not found
-    const result = await User.aggregate(shipping_address_aggregation_pipeline);
-
-    //console.log("Shipping Address returned from getShippingAddressById ", result);
-    console.log("result ", result);
-
-    if (result.length > 0) {
-        // Resources Found
-        // Return the shipping addresses in the Shipping Addresses array.
-        // Here, 200 is used since the resource is found and returned successfully at the requested URL (no redirect).
-        res.status(200).json(result);
-    }
-
-    else {
-        const shipping_address_not_found_error = new ResourceNotFoundError(`Could not find the shipping addresse(s) with ${request_body} since they do not exist.`);
-        throw shipping_address_not_found_error;
-    }
+    res.status(200).json(result);
 
     console.log("===END OF searchShippingAddress===");
 
@@ -298,18 +217,25 @@ const deleteShippingAddressById = asyncErrorHandler(async (req, res, next) => {
         throw resource_not_found_error;
     }
 
+    const shipping_address_id = request_body_deep_clone.shipping_address_id;
 
-    //const shipping_address_id = request_body.shipping_address_id;
-
-    /*if(checkShippingAddressExists(request_body) === false) {
+    console.log("Checking if the shipping address to be deleted exists.");
+    if(await checkShippingAddressExists(req) === false) {
         const shipping_address_not_found_error = new ResourceNotFoundError(`Could not delete the shipping address with ${shipping_address_id} since it does not exist.`);
         throw shipping_address_not_found_error;
-    }*/
+    }
 
-    const result = await User.updateOne({ user_id: user_id }, { $pull: { "ShippingAddresses": request_body_deep_clone } });
-    console.log("result ", result);
+    const filter = {user_id: user_id, "ShippingAddresses.shipping_address_id": shipping_address_id};
+    console.log("filter ", filter);
 
-    // Here 200 is used as status code since the server wants to send a message after deletion.
+    const delete_query = {shipping_address_id: shipping_address_id};
+    console.log("delete_query ", delete_query);
+
+    const result = await User.findOneAndUpdate(filter, { $pull: { ShippingAddresses: delete_query } }, {new: true}, {runValidators: true}).lean();
+    
+    console.log("result in deleteShippingAddressById ", result);
+
+    
     res.status(200).json(result);
 
     console.log("===END OF deleteShippingAddressById===");

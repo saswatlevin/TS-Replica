d20f7011f68fe83fe199d80771192b6402b05350
Saswat Levin <saswatlevin@gmail.com>
2026-05-12 10:10:33 +0530

    Moved updateCartItemName, updateCartItemPrice, updateCartItemDisocunt, updateCartItemImageURI and calculateAndUpdateCartItemTotals from cartItemControllers.js to cartItemSupportFunctions.js

--- Website/src/Middleware/Controllers/SupportFunctions/cartItemSupportFunctions.js
+++ Website/src/Middleware/Controllers/SupportFunctions/cartItemSupportFunctions.js
@@ -107,11 +107,256 @@ const getCartItemTotalAndDiscountPercentage = async(req) => {
         console.log("Error in getCartItemTotal ", error);
         throw error;
     }
-}
+};
 
+
+const updateCartItemPrice = async (req, res) => {
+    console.log("In updateCartItemPrice (HELPER FUNCTION)");
+
+    try {
+
+            const product_id = req.body.product_id;
+            // console.log("product_id ", product_id);
+            const updated_product_price = res.locals.updated_product_price;
+            // console.log("updated_product_price ", updated_product_price);
+
+            const build_update_cart_item_price_pipeline = buildUpdateCartItemPricePipeline(product_id,updated_product_price);
+
+            const result = await User.updateMany({ "CartItems.product_id": product_id }, build_update_cart_item_price_pipeline);
+
+            console.log("result in updateCartItemPrice ", result);
+
+            console.log("===END OF updateCartItemPrice===");
+
+            return result;
+        
+    }
+
+    catch (error) {
+        console.log("Error in updateCartItemPrice ", error);
+        throw error;
+    }
+};
+
+const updateCartItemDiscount = async (req, res) => {
+    console.log("In updateCartItemDiscount (HELPER FUNCTION)");
+
+    try {
+
+        const product_id = req.body.product_id;
+        // console.log("product_id ", product_id);
+            
+        const updated_discount_code = res.locals.updated_discount_code;
+        // console.log("updated_discount_code ", updated_disocunt_code);
+
+        const updated_discount_percentage = res.locals.updated_discount_code;
+        // console.log("updated_discount_code ", updated_disocunt_code);
+
+        const build_update_cart_item_discount_pipeline = buildUpdateCartItemDiscountPipeline(product_id, updated_discount_code, updated_discount_percentage);
+
+        const result = await User.updateMany({ "CartItems.product_id": product_id }, build_update_cart_item_price_pipeline);
+
+        console.log("result in updateCartItemDiscount ", result);
+        
+        console.log("===END OF updateCartItemDiscount===");
+
+        return result;
+        
+    }
+
+    catch (error) {
+        console.log("Error in updateCartItemDiscount ", error);
+        throw error;
+    }
+};
+
+const updateCartItemName = async (req) => {
+    console.log("In updateCartItemName (HELPER FUNCTION)");
+
+    try {
+
+        //const user_id = req.params.user_id;
+        //console.log("user_id ", user_id);
+
+        //const cart_item_id = req.body.cart_item_id;
+        //console.log("cart_item_id ", cart_item_id);
+
+        const product_id = req.body.product_id;
+        console.log("product_id ", product_id);
+
+        /*console.log("Checking if the user exists");
+        if (await checkUserExists(req) === false) {
+            const user_id_not_found_error = new ResourceNotFoundError(`Could not update the Cart Item since the user with user_id ${user_id} does not exist.`);
+            throw user_id_not_found_error;
+        }
+
+        console.log("Checking if the cart item exists");
+        if (await checkCartItemExists(req) === false) {
+            const cart_item_not_found_error = new ResourceNotFoundError(`Could not update the Cart Item since the cart item with cart_item_id ${cart_item_id} does not exist.`);
+            throw cart_item_not_found_error;
+        }*/
+        
+        // We use only user_id and product_id since multiple cart items of the same product (different cart_item_id AND different sku) must have their product_name updated.
+        const filter = { "CartItems.product_id": product_id };
+
+        console.log("filter ", filter);
+
+        const updated_cart_item_name = res.locals.updated_product_name;
+        console.log("updated_cart_item_name ", updated_cart_item_name);
+
+        const update_object = {
+            $set: {
+                "CartItems.$[item].cart_item_name": updated_cart_item_name
+            }
+        };
+
+        console.log("update_object ", update_object);
+
+        // Update the product_name of all product items (cart items) of the same product if present.
+        const result = await User.updateMany(filter, update_object, { arrayFilters: [{ "item.product_id": product_id }], runValidators: true });
+
+        console.log("result in updateCartItemName ", result);
+
+        console.log("===END OF updateCartItemName===");
+        
+        return result;
+    }
+
+    catch (error) {
+        console.log("Error in updateCartItemName ", error);
+        throw error;
+    }
+};
+
+const updateCartItemImageURI = async (req) => {
+    console.log("In updateCartItemImageURI (HELPER FUNCTION)");
+
+    try {
+
+        //const user_id = req.params.user_id;
+        //console.log("user_id ", user_id);
+
+        // The cart_item_id is needed for checkCartItemExists().
+        //const cart_item_id = req.body.cart_item_id;
+        //console.log("cart_item_id ", cart_item_id);
+
+        const product_id = req.body.product_id;
+        console.log("product_id ", product_id);
+
+        /*console.log("Checking if the user exists");
+        if (await checkUserExists(req) === false) {
+            const user_id_not_found_error = new ResourceNotFoundError(`Could not update the Cart Item since the user with user_id ${user_id} does not exist.`);
+            throw user_id_not_found_error;
+        }
+
+        console.log("Checking if the cart item exists");
+        if (await checkCartItemExists(req) === false) {
+            const cart_item_not_found_error = new ResourceNotFoundError(`Could not update the Cart Item since the cart item with cart_item_id ${cart_item_id} does not exist.`);
+            throw cart_item_not_found_error;
+        }*/
+        
+        const filter = { "CartItems.product_id": product_id };
+        console.log("filter ", filter);
+
+        const updated_cart_item_image_uri = res.locals.updated_cart_item_image_uri;
+        console.log("updated_cart_item_image_uri ", updated_cart_item_image_uri);
+
+        const update_object = {
+            $set: {
+                "CartItems.$[item].cart_item_image_uri": updated_cart_item_image_uri
+            }
+        };
+
+        console.log("update_object ", update_object);
+
+        const result = await User.updateMany(filter, update_object, { arrayFilters: [{ "item.product_id": product_id }], runValidators: true });
+
+        console.log("result in updateCartItemImageURI ", result);
+
+        console.log("===END OF updateCartItemImageURI===");
+        
+        return result;
+    }
+
+    catch (error) {
+        console.log("Error in updateCartItemImageURI ", error);
+        throw error;
+    }
+};
+
+const calculateAndUpdateCartItemTotals = async(req) => {
+
+   // Get all the CartItem sub-documents from the CartItems array of the respective 
+   // user and add sum the respective fields and then update them in the respective user document.
+   console.log("In calculateAndUpdateCartItemTotals");
+
+   try {
+
+        const user_id = req.params.user_id;
+        console.log("user_id ", user_id);
+
+        const user_id_query = {user_id: user_id};
+        console.log("user_id_query ", user_id_query);
+
+        const user = await User.findOne(user_id_query).lean();
+
+        const cart_items = user.CartItems;
+
+        const cart_item_length = cart_items.length;
+
+        var total_item_total = 0;
+   
+        var total_discount_amount = 0;
+   
+        var total_discounted_total = 0;
+
+        var total_discount_percentage = 0;
+
+        var total_payable_amount = 0;
+
+        var i = 0;
+
+        for ( i = 0; i < cart_item_length; ++i) {
+
+            total_item_total = total_item_total + cart_items[i].item_total;
+            total_discount_amount = total_discount_amount + cart_items[i].discount_amount;
+            total_discounted_total = total_discounted_total + cart_items[i].discounted_total;
+
+        } 
+
+        total_discount_percentage = (total_discount_amount / total_item_total) * 100;
+        total_payable_amount = total_item_total - total_discount_amount;
+
+        const update_object = { 
+            
+            total_item_total: total_item_total, 
+            total_discount_amount: total_discount_amount, 
+            total_discounted_total: total_discounted_total, 
+            total_discount_percentage: total_discount_percentage, 
+            total_payable_amount: total_payable_amount
+        };
+
+        console.log("update_object ", update_object);
+
+        const result = User.findOneAndUpdate(user_id_query, update_object, {new: true}, {runValidators: true}).lean();
+
+        console.log("result in calculateCartItemTotals ", result);
+        return result;
+   }
+
+   catch(error) {
+        console.log("Error in calculateCartItemTotals ", error);
+        throw error;
+   }
+};
 
 module.exports = {
     checkCartItemExists,
     checkIsCartFull,
-    getCartItemTotalAndDiscountPercentage
+    getCartItemTotalAndDiscountPercentage,
+    updateCrtItemPrice,
+    updateCartItemDiscount,
+    updateCartItemName,
+    updateCartItemImageURI,
+    calculateAndUpdateCartItemTotals
 };
--- Website/src/Middleware/Controllers/cartItemControllers.js
+++ Website/src/Middleware/Controllers/cartItemControllers.js
@@ -14,6 +14,8 @@ const { buildUpdateCartItemPricePipeline } = require('../AggregationPipelines/ca
 const { buildUpdateCartItemDiscountPipeline } = require('../AggregationPipelines/cartItemAggregationPipelines');
 const { IllegalUpdateError } = require('../OperationalErrors/IllegalUpdateError');
 
+const { calculateAndUpdateCartItemTotals } = require('./SupportFunctions/cartItemSupportFunctions');
+
 const createCartItem = asyncErrorHandler(async (req, res, next) => {
     console.log("In createCartItem");
 
@@ -95,180 +97,6 @@ const createCartItem = asyncErrorHandler(async (req, res, next) => {
     console.log("===END OF createCartItem===")
 });
 
-const updateCartItemPrice = async (req, res) => {
-    console.log("In updateCartItemPrice (HELPER FUNCTION)");
-
-    try {
-
-            const product_id = req.body.product_id;
-            // console.log("product_id ", product_id);
-            const updated_product_price = res.locals.updated_product_price;
-            // console.log("updated_product_price ", updated_product_price);
-
-            const build_update_cart_item_price_pipeline = buildUpdateCartItemPricePipeline(product_id,updated_product_price);
-
-            const result = await User.updateMany({ "CartItems.product_id": product_id }, build_update_cart_item_price_pipeline);
-
-            console.log("result in updateCartItemPrice ", result);
-
-            console.log("===END OF updateCartItemPrice===");
-
-            return result;
-        
-    }
-
-    catch (error) {
-        console.log("Error in updateCartItemPrice ", error);
-        throw error;
-    }
-};
-
-const updateCartItemDiscount = async (req, res) => {
-    console.log("In updateCartItemDiscount (HELPER FUNCTION)");
-
-    try {
-
-        const product_id = req.body.product_id;
-        // console.log("product_id ", product_id);
-            
-        const updated_discount_code = res.locals.updated_discount_code;
-        // console.log("updated_discount_code ", updated_disocunt_code);
-
-        const updated_discount_percentage = res.locals.updated_discount_code;
-        // console.log("updated_discount_code ", updated_disocunt_code);
-
-        const build_update_cart_item_discount_pipeline = buildUpdateCartItemDiscountPipeline(product_id, updated_discount_code, updated_discount_percentage);
-
-        const result = await User.updateMany({ "CartItems.product_id": product_id }, build_update_cart_item_price_pipeline);
-
-        console.log("result in updateCartItemDiscount ", result);
-        
-        console.log("===END OF updateCartItemDiscount===");
-
-        return result;
-        
-    }
-
-    catch (error) {
-        console.log("Error in updateCartItemDiscount ", error);
-        throw error;
-    }
-};
-
-const updateCartItemName = async (req) => {
-    console.log("In updateCartItemName (HELPER FUNCTION)");
-
-    try {
-
-        //const user_id = req.params.user_id;
-        //console.log("user_id ", user_id);
-
-        //const cart_item_id = req.body.cart_item_id;
-        //console.log("cart_item_id ", cart_item_id);
-
-        const product_id = req.body.product_id;
-        console.log("product_id ", product_id);
-
-        /*console.log("Checking if the user exists");
-        if (await checkUserExists(req) === false) {
-            const user_id_not_found_error = new ResourceNotFoundError(`Could not update the Cart Item since the user with user_id ${user_id} does not exist.`);
-            throw user_id_not_found_error;
-        }
-
-        console.log("Checking if the cart item exists");
-        if (await checkCartItemExists(req) === false) {
-            const cart_item_not_found_error = new ResourceNotFoundError(`Could not update the Cart Item since the cart item with cart_item_id ${cart_item_id} does not exist.`);
-            throw cart_item_not_found_error;
-        }*/
-        
-        // We use only user_id and product_id since multiple cart items of the same product (different cart_item_id AND different sku) must have their product_name updated.
-        const filter = { "CartItems.product_id": product_id };
-
-        console.log("filter ", filter);
-
-        const updated_cart_item_name = res.locals.updated_product_name;
-        console.log("updated_cart_item_name ", updated_cart_item_name);
-
-        const update_object = {
-            $set: {
-                "CartItems.$[item].cart_item_name": updated_cart_item_name
-            }
-        };
-
-        console.log("update_object ", update_object);
-
-        // Update the product_name of all product items (cart items) of the same product if present.
-        const result = await User.updateMany(filter, update_object, { arrayFilters: [{ "item.product_id": product_id }], runValidators: true });
-
-        console.log("result in updateCartItemName ", result);
-
-        console.log("===END OF updateCartItemName===");
-        
-        return result;
-    }
-
-    catch (error) {
-        console.log("Error in updateCartItemName ", error);
-        throw error;
-    }
-};
-
-const updateCartItemImageURI = async (req) => {
-    console.log("In updateCartItemImageURI (HELPER FUNCTION)");
-
-    try {
-
-        //const user_id = req.params.user_id;
-        //console.log("user_id ", user_id);
-
-        // The cart_item_id is needed for checkCartItemExists().
-        //const cart_item_id = req.body.cart_item_id;
-        //console.log("cart_item_id ", cart_item_id);
-
-        const product_id = req.body.product_id;
-        console.log("product_id ", product_id);
-
-        /*console.log("Checking if the user exists");
-        if (await checkUserExists(req) === false) {
-            const user_id_not_found_error = new ResourceNotFoundError(`Could not update the Cart Item since the user with user_id ${user_id} does not exist.`);
-            throw user_id_not_found_error;
-        }
-
-        console.log("Checking if the cart item exists");
-        if (await checkCartItemExists(req) === false) {
-            const cart_item_not_found_error = new ResourceNotFoundError(`Could not update the Cart Item since the cart item with cart_item_id ${cart_item_id} does not exist.`);
-            throw cart_item_not_found_error;
-        }*/
-        
-        const filter = { "CartItems.product_id": product_id };
-        console.log("filter ", filter);
-
-        const updated_cart_item_image_uri = res.locals.updated_cart_item_image_uri;
-        console.log("updated_cart_item_image_uri ", updated_cart_item_image_uri);
-
-        const update_object = {
-            $set: {
-                "CartItems.$[item].cart_item_image_uri": updated_cart_item_image_uri
-            }
-        };
-
-        console.log("update_object ", update_object);
-
-        const result = await User.updateMany(filter, update_object, { arrayFilters: [{ "item.product_id": product_id }], runValidators: true });
-
-        console.log("result in updateCartItemImageURI ", result);
-
-        console.log("===END OF updateCartItemImageURI===");
-        
-        return result;
-    }
-
-    catch (error) {
-        console.log("Error in updateCartItemImageURI ", error);
-        throw error;
-    }
-};
-
 const updateCartItemQuantity = asyncErrorHandler(async(req, res, next) => {
     console.log("In updateCartItemQuantity");
     
@@ -333,11 +161,13 @@ const updateCartItemQuantity = asyncErrorHandler(async(req, res, next) => {
         };
     console.log("update_object ", update_object);
 
-    const result = await User.findOneAndUpdate(filter, update_object, {new: true}, {runValidators: true}).lean();
+    const result_1 = await User.findOneAndUpdate(filter, update_object, {new: true}, {runValidators: true}).lean();
 
-    console.log("result ", result);
+    console.log("result_1 in updateCartItemQuantity ", result_1);
 
-    res.status(200).json(result);
+    const result_2 = await calculateAndUpdateCartItemTotals(req);
+
+    res.status(200).json(result_1);
 
     console.log("===END OF updateCartItemQuantity===");
 });
@@ -458,84 +288,10 @@ const deleteCartItem = asyncErrorHandler(async(req, res, next) => {
 
 });
 
-const calculateAndUpdateCartItemTotals = async(req) => {
-
-   // Get all the CartItem sub-documents from the CartItems array of the respective 
-   // user and add sum the respective fields and then update them in the respective user document.
-   console.log("In calculateAndUpdateCartItemTotals");
-
-   try {
-
-        const user_id = req.params.user_id;
-        console.log("user_id ", user_id);
-
-        const user_id_query = {user_id: user_id};
-        console.log("user_id_query ", user_id_query);
-
-        const user = await User.findOne(user_id_query).lean();
-
-        const cart_items = user.CartItems;
-
-        const cart_item_length = cart_items.length;
-
-        var total_item_total = 0;
-   
-        var total_discount_amount = 0;
-   
-        var total_discounted_total = 0;
-
-        var total_discount_percentage = 0;
-
-        var total_payable_amount = 0;
-
-        var i = 0;
-
-        for ( i = 0; i < cart_item_length; ++i) {
-
-            total_item_total = total_item_total + cart_items[i].item_total;
-            total_discount_amount = total_discount_amount + cart_items[i].discount_amount;
-            total_discounted_total = total_discounted_total + cart_items[i].discounted_total;
-
-        } 
-
-        total_discount_percentage = (total_discount_amount / total_item_total) * 100;
-        total_payable_amount = total_item_total - total_discount_amount;
-
-        const update_object = { 
-            
-            total_item_total: total_item_total, 
-            total_discount_amount: total_discount_amount, 
-            total_discounted_total: total_discounted_total, 
-            total_discount_percentage: total_discount_percentage, 
-            total_payable_amount: total_payable_amount
-        };
-
-        console.log("update_object ", update_object);
-
-        const result = User.findOneAndUpdate(user_id_query, update_object, {new: true}, {runValidators: true}).lean();
-
-        console.log("result in calculateCartItemTotals ", result);
-        return result;
-   }
-
-   catch(error) {
-        console.log("Error in calculateCartItemTotals ", error);
-        throw error;
-   }
-   
-
-    
-};
-
 module.exports = {
     createCartItem,
-    updateCartItemPrice,
-    updateCartItemDiscount,
-    updateCartItemName,
-    updateCartItemImageURI,
     updateCartItemQuantity,
     searchCartItem,
     getCartItemById,
-    deleteCartItem,
-    calculateAndUpdateCartItemTotals
+    deleteCartItem
 };

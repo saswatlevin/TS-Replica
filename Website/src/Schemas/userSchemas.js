654775cc214eed2f7563171676be3ce21f9b650d
Saswat Levin <saswatlevin@gmail.com>
2026-05-15 01:45:42 +0530

    Added some test code for registerUserResponseSchema in userSchemas.js. Server is running and user is being created

--- Website/src/Schemas/userSchemas.js
+++ Website/src/Schemas/userSchemas.js
@@ -133,9 +133,9 @@ const registerUserResponseSchema = z.object({
 
     phone_number: customValidators.zodIsMobilePhone,
 
-    first_name: z.string("The first_name field must be a string.").min(1, { message: "The first_name field is a required field." }).max(100, { message: "The first_name field has a maximum permitted length of 100 characters." }).regex(customValidators.nameRegex, { message: "The first_name field can only contain uupercase letters, lowercase letters, hyphens and single-quotes." }),
+    first_name: z.string("The first_name field must be a string.").min(1, { message: "The first_name field is a required field." }).max(100, { message: "The first_name field has a maximum permitted length of 100 characters." }).regex(customValidators.nameRegex, { message: "The first_name field can only contain uppercase letters, lowercase letters, hyphens and single-quotes." }),
 
-    last_name: z.string("The last_name field must be a string.").min(1, { message: "The last_name field is a required field." }).max(100, { message: "The last_name field has a maximum permitted length of 100 characters." }).regex(customValidators.nameRegex, { message: "The last_name field can only contain uupercase letters, lowercase letters, hyphens and single-quotes." }),
+    last_name: z.string("The last_name field must be a string.").min(1, { message: "The last_name field is a required field." }).max(100, { message: "The last_name field has a maximum permitted length of 100 characters." }).regex(customValidators.nameRegex, { message: "The last_name field can only contain uppercase letters, lowercase letters, hyphens and single-quotes." }),
 
     user_role: z.enum(["user", "admin"], { message: "The user_role field is a required field. It takes 1 of the following values: user, admin." }),
 
@@ -183,11 +183,74 @@ module.exports = {
     userResponseSchemaArray
 };
 
-// TESTS
-// console.log(testUserData);
-// const result = userRequestSchema.safeParse(testRequestUserDataFull);
-// console.log(" User Request Schema Test Result ", result);
-// console.log(" User Request Schema Test Result Errors ", result?.error?.issues);
+/* TESTS
+testUserData = {
+  "user_id": '4bade49c4a60',
+  "docType": 'USER',
+  "date_created_at": '2026-05-14T18:39:49',
+  "email": 'els001@server.com',
+  "password": '$argon2id$v=19$m=65536,t=3,p=4$ghl7cmGOJIReLv8V3+o3WQ$dTzfVf4RdBSuqHiuCF6/PjujtntZJYhgINrlxVnrOac',
+  "phone_number": '918297161384',
+  "first_name": 'ELSOne',
+  "last_name": 'SLEOne',
+  "user_role": 'user',
+  "upper_size_number": 40,
+  "upper_size_letter": 'L',
+  "others_size_letter": 'L',
+  "email_comms_type": 'One weekly recap',
+  "sms_comms": true,
+  "ShippingAddresses": [],
+  "CartItems": [],
+  "total_item_total": 0,
+  "total_discount_amount": 0
+}
+
+testUserData2 = {
+  "user_id": '4bade49c4a60',
+  "docType": 'USER',
+  "date_created_at": '2026-05-14T18:39:49',
+  "email": 'els001@server.com',
+  "password": '$argon2id$v=19$m=65536,t=3,p=4$ghl7cmGOJIReLv8V3+o3WQ$dTzfVf4RdBSuqHiuCF6/PjujtntZJYhgINrlxVnrOac',
+  "phone_number": '918297161384',
+  "first_name": 'ELSOne',
+  "last_name": 'SLEOne',
+  "user_role": 'user',
+  "upper_size_number": 40,
+  "upper_size_letter": 'L',
+  "others_size_letter": 'L',
+  "email_comms_type": 'One weekly recap',
+  "sms_comms": true,
+  "ShippingAddresses": [],
+  "CartItems": [],
+  "total_item_total": 0,
+  "total_discount_amount": 0,
+  "total_discounted_total": 0,
+  "total_discount_percentage": 0,
+  "total_payable_amount": 0,
+  "_id": '6a0616f54b6344ba32b83953',
+  "__v": 0
+}
+
+//console.log(testUserData);
+console.log({
+    zodIsISO8601: customValidators.zodIsISO8601,
+    zodIsEmail: customValidators.zodIsEmail,
+    zodIsPasswordHash: customValidators.zodIsPasswordHash,
+    zodIsMobilePhone: customValidators.zodIsMobilePhone,
+
+    zodIsUpperSizeNumber: productItemValidators.zodIsUpperSizeNumber,
+    zodIsUpperSizeLetter: productItemValidators.zodIsUpperSizeLetter,
+    zodIsOthersSizeLetter: productItemValidators.zodIsOthersSizeLetter,
+
+    shippingAddressArrayZeroSchema,
+    cartItemArrayZeroSchema,
+    objectIdSchema
+});
+
+
+const result = registerUserResponseSchema.parse(testUserData);
+//console.log(" User Request Schema Test Result ", result);
+//console.log(" User Request Schema Test Result Errors ", result?.error?.issues); */
 
 
 

b0a0e1325963793c647799418615b57e6da01e8c
Saswat Levin <saswatlevin@gmail.com>
2026-06-20 12:25:13 +0530

    Removed the updateproductimageuri route and added multer file upload middleware to the createproductimage route in productImageRoutes.js.

--- Website/src/Routes/productImageRoutes.js
+++ Website/src/Routes/productImageRoutes.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const productImageControllers = require('../Controllers/productImageControllers');
 const requestValidator = require('../Validators/requestValidator');
 const productImageSchemas = require('../Schemas/productImageSchemas');
+const { upload } = require('../FileUploader/fileUploader');
 const { xss } = require('express-xss-sanitizer');
 
-router.route('/createproductimage').post(requestValidator(productImageSchemas.createProductImageSchema), xss(), productImageControllers.createProductImage);
-
-router.route('/updateproductimageuri/:user_id').patch(requestValidator(productImageSchemas.updateProductImageURISchema), xss(), productImageControllers.updateProductImageURI);
+router.route('/createproductimage').post(upload.single('image'), requestValidator(productImageSchemas.createProductImageSchema), xss(), productImageControllers.createProductImage);
 
 router.route('/searchproductimagebyid').get(requestValidator(productImageSchemas.searchProductImageByIdSchema), xss(), productImageControllers.searchProductImageById);
 

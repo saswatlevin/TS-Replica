d7978d2f989a8875ab9abeb3393b1210b221a8c2
Saswat Levin <saswatlevin@gmail.com>
2026-06-20 12:03:50 +0530

    Added file upload capability and signiifcantly changed createProductImage and deleteProductImage by adding MongoDB Transactions to them. Removed updateProductImageURI in productImageControllers.js.

--- Website/src/Controllers/productImageControllers.js
+++ Website/src/Controllers/productImageControllers.js
@@ -7,6 +7,7 @@ const EmptyRequestBodyError = require('../OperationalErrors/EmptyRequestBodyErro
 const RedundantUpdateError = require('../OperationalErrors/RedundantUpdateError');
 const IllegalUpdateError = require('../OperationalErrors/IllegalUpdateError');
 const DuplicateSubDocumentError = require('../OperationalErrors/DuplicateSubDocumentError');
+const InvalidFileTypeError = require('../OperationalErrors/InvalidFileTypeError');
 const createRandomString = require('../createRandomString');
 
 const { checkProduct } = require('./SupportFunctions/productSupportFunctions');
@@ -21,133 +22,124 @@ const { updateCartItemImageURI } = require('./SupportFunctions/cartItemSupportFu
 
 const { checkIsEmptyObject } = require('./SupportFunctions/userSupportFunctions');
 
+const { getProductImageURI } = require('./SupportFunctions/productImageSupportFunctions');
+
+const { getProductImageFlag } = require('./SupportFunctions/productImageSupportFunctions');
+
+const sharp = require('sharp');
+const { fileTypeFromFile } = require('file-type');
+const fs = require('fs');
+const { IMAGE_DESTINATION_PATH } = require('../FileUploader/fileUploader');
+const path = require('path');
+
+// List of trusted MIME types
+const ALLOWED_MIME = new Set(['image/jpeg', 'image/png', 'image/webp']);
 
 const createProductImage = asyncErrorHandler(async(req, res, next) => {
     console.log("In createProductImage");
 
+    console.log("Checking if the image upload succeeded.");
+    if (req?.file === null || req?.file === undefined) {
+        throw new EmptyRequestBodyError(`No image file was uploaded.`);
+    }
+
+    const file_path = req.file.path;
+    console.log("Filepath of uploaded image ", file_path);
+
+    // Validate the file's REAL type by inspecting its binary content
+    console.log("Validating the file's REAL type by inspecting its binary content.");
+    const type = await fileTypeFromFile(file_path);
+
+    // If the file isn't of the required type, delete it.
+    console.log("Checking if the file is of the required type.");
+    if (!type || !ALLOWED_MIME.has(type.mime)) {
+        console.log("The file isn't of the required type and so has been deleted from ", file_path);
+        fs.unlinkSync(file_path);
+        throw new InvalidFileTypeError(`Uploaded file is not a valid image.`);
+    }
+
     console.log("\n Checking if the request_body is empty.");
     if (checkIsEmptyObject(req) === true) {
         const empty_request_body_error = new EmptyRequestBodyError(`Could not create Product Image for Product with product_id ${product_id} as the request body is empty.`);
-
+        console.log("File deleted since request_body is empty from ", file_path);
+        fs.unlinkSync(file_path); // Delete the file since the request body is empty. 
         throw empty_request_body_error;
     }
 
     const product_id = req.body.product_id;
-    console.log("product_id ", product_id);
 
     console.log("\nChecking if the Product exists");
     if (await checkProduct(req) === false) {
         const product_not_found_error = new ResourceNotFoundError(`Could not find Product document with product_id ${product_id}`);
+        console.log("File deleted since the Product document doesn't exist from ", file_path);
+        fs.unlinkSync(file_path);  // Delete the file since the Product doesn't exist
+
         throw product_not_found_error;
     }
 
-    console.log("Checking if the product image already exists");
-    if (await checkDuplicateProductImageExists(req) === true) {
-        const product_image_already_exists_error = new DuplicateSubDocumentError(`Could not create the Product Image for the Product Document with ${product_id} as it already exists.`);
-        throw product_image_already_exists_error;
-    }
-
     const image_id = createRandomString(6);
-    console.log("Generated image_id ", image_id);
 
-    const image_uri = req.body.image_uri;
-    console.log("image_uri ", image_uri);
-
-    var main_image = undefined;
-
-    console.log("Checking if the product image array is empty or not");
-    if (await getProductImageArrayLength(req) > 0) {
-        main_image = false;
-    }
-
-    else {
-        main_image = true;
-    }
-
-    console.log("main_image ", main_image);
-
-    const product_image = {image_id, image_uri, main_image: main_image};
-    console.log("product_image ", product_image);
+    const final_file_name = `${image_id}.jpg`;
+    const final_path = path.join(IMAGE_DESTINATION_PATH, final_file_name);
     
-    // A live mongoose document contains many hidden keys and fields. 
-    // The .lean() functions strips these keys and fields completely.
-    const result = await Product.findOneAndUpdate({ product_id: product_id }, { $push: { product_images: product_image } }, {new: true, runValidators: true}).lean(); // return the updated document;
+    const mongodb_transaction_session = await mongoose.startSession();
+
+    let result;
+
+    try{
+        await mongodb_transaction_session.withTransaction(async () => {
+            console.log("Processing the Uploaded Image");
+
+            var main_image = null;
+            
+            console.log("Checking if the product image array is empty");
+            if (await getProductImageArrayLength(req) > 0) {
+                main_image = false;
+            }
+
+            else {
+                main_image = true;
+            }
+
+            console.log("Checking if the product image array is full");
+            if (await getProductImageArrayLength(req) >= 5) {
+                const error = new IllegalUpdateError(`Cannot create product_image for product with product_id ${product_id} since it already has 5 images.`); 
+                console.log("Deleting the original file at ", file_path);
+                fs.unlinkSync(file_path);
+                throw error;
+            }
+
+            console.log("Re-encoding the file to remove malicious payloads and creating the image at destination.");
+            await sharp(file_path).resize({width: 2000, height: 2000, fit: 'inside', withoutEnlargement: true}).toFormat('jpeg', { quality: 85 }).withMetadata(false).toFile(final_path);                  
+            // Delete the original (unverified) upload
+            console.log("Deleting the original file at ", file_path);
+            fs.unlinkSync(file_path);
+
+            const image_uri = final_path;
+            console.log("image_uri (final_path) ", image_uri);
+
+            //console.log("main_image ", main_image);
+
+            const product_image = {image_id, image_uri, main_image: main_image};
+            console.log("product_image ", product_image);
+    
+            // A live mongoose document contains many hidden keys and fields. 
+            // The .lean() functions strips these keys and fields completely.
+            result = await Product.findOneAndUpdate({ product_id: product_id }, { $push: { product_images: product_image } }, {new: true, runValidators: true, session: mongodb_transaction_session}).lean(); 
         
-    console.log("Product image created successfully, result ", result);
+            console.log("Product image created successfully, result ", result);
+        })
+    }
+
+    finally {
+        mongodb_transaction_session.endSession();
+    }
+
+
     // Here, we use 201 since it means that the resource has been created.
     res.status(201).json(result);
 
     console.log("===END OF createProductImage===");
-});
-
-
-const updateProductImageURI = asyncErrorHandler(async(req, res, next) => {
-    console.log("In updateProductImageURI");
-
-    const product_id = req.body.product_id;
-    console.log("product_id ", product_id);
-
-    console.log("\nChecking if the request_body is empty.");
-    if (checkIsEmptyObject(req) === true) {
-        const empty_request_body_error = new EmptyRequestBodyError(`Could not update the image_uri for the Product Image of Product with product_id ${product_id} as the request body is empty.`);
-
-        throw empty_request_body_error;
-    }
-
-    console.log("\nChecking if the Product exists.");
-    if (await checkProduct(req) === false) {
-        const product_not_found_error = new ResourceNotFoundError(`Could not find Product document with product_id ${product_id}`);
-
-        throw product_not_found_error;
-    }
-
-    const image_id = req.body.image_id;
-    console.log("image_id ", image_id);
-
-    console.log("\nChecking if the Product Image to be updated exists");
-    if (await checkProductImageExists(req) === false) {
-        const product_image_not_found_error = new ResourceNotFoundError(`Could not update the Product Image URI since the Product Image sub-document with image_id ${image_id} wasn't found in the Product document with product_id ${product_id}`);
-
-        throw product_image_not_found_error;
-    }
-
-    const updated_image_uri = req.body.image_uri;
-    console.log("updated_image_uri ", updated_image_uri);
-
-    console.log("Checking if the image URI to be updated already exists");
-    if (await checkDuplicateProductImageExists(req) === true) {
-        const redundant_update_error = new RedundantUpdateError(`Could not update the Product Image URI ${updated_image_uri} since it already exists for the product image with image_id ${image_id}`);
-
-        throw redundant_update_error;
-    }
-
-    const filter = {product_id: product_id, "product_images.image_id": image_id};
-    console.log("filter ", filter);
-
-    const update_object = {
-            $set: {
-                "product_images.$.image_uri": updated_image_uri
-        }
-    };
-
-    const result_1 = await Product.findOneAndUpdate(filter, update_object, {new: true, runValidators: true}).lean();
-
-    console.log("result in updateProductImagePath ", result_1);
-
-    const main_image = req.body.main_image;
-    console.log("main_image ", main_image);
-
-    res.locals.updated_cart_item_image_uri = updated_image_uri;
-
-    if (main_image === true) {
-        console.log("Updating the Image URI of the corresponding cart items containing the product image URI.")
-        const result_2 = await updateCartItemImageURI(req);
-    }
-
-    console.log("result of updateProductImageURI ", result_1);
-
-    res.status(200).json(result_1);
-    console.log("===END OF updateProductImageURI===");
 });
 
 
@@ -165,13 +157,6 @@ const searchProductImageById = asyncErrorHandler(async(req, res, next) => {
         const empty_request_body_error = new EmptyRequestBodyError(`Could not search for the Product Image of Product with product_id ${product_id} as the request body is empty.`);
 
         throw empty_request_body_error;
-    }
-
-    console.log("Checking if the Product exists");
-    if (await checkProduct(req) === false) {
-        const product_not_found_error = new ResourceNotFoundError(`Could not find Product document with product_id ${product_id}`);
-
-        throw product_not_found_error;
     }
 
     const search_query = {product_id: product_id, "product_images.image_id": image_id};
@@ -192,28 +177,33 @@ const deleteProductImage = asyncErrorHandler(async(req, res, next) => {
     const product_id = req.body.product_id;
     console.log("product_id ", product_id);
 
-    console.log("Checking if the request body is empty");
+    console.log("Checking if the request body is empty.");
     if (checkIsEmptyObject(req) === true) {
         const empty_request_body_error = new EmptyRequestBodyError(`Could not delete the Product Image of Product with product_id ${product_id} as the request body is empty.`);
 
         throw empty_request_body_error;
     }
 
-    console.log("Checking if the Product exists");
-    if (await checkProduct(req) === false) {
-        const product_not_found_error = new ResourceNotFoundError(`Could not find Product document with product_id ${product_id}`);
-
-        throw product_not_found_error;
-    }
-
     const image_id = req.body.image_id;
     console.log("image_id ", image_id);
 
-    console.log("\nChecking if the Product Image to be deleted exists");
+    console.log("\nChecking if the Product exists");
+    if (await checkProduct(req) === false) {
+        const product_not_found_error = new ResourceNotFoundError(`Could not delete the Product Image since the Product with product_id ${product_id} does not exist.`);
+        throw product_not_found_error;
+    }
+
+    console.log("\nChecking if the Product Image to be deleted exists.");
     if (await checkProductImageExists(req) === false) {
         const product_image_not_found_error = new ResourceNotFoundError(`Could not delete the Product Image since the Product Image sub-document with image_id ${image_id} wasn't found in the Product document with product_id ${product_id}`);
 
         throw product_image_not_found_error;
+    }
+
+    console.log("Checking if the product_image to be deleted is the main_image");
+    if (await getProductImageFlag(req) === true) {
+        const error = new IllegalUpdateError(`Cannot delete product_image with image_id ${image_id} since it is the main image`);
+        throw error;
     }
 
     const filter = {product_id: product_id};
@@ -226,19 +216,31 @@ const deleteProductImage = asyncErrorHandler(async(req, res, next) => {
     };
 
 
-    const main_image = req.body.main_image;
-    console.log("main_image value ", main_image);
+    const mongodb_transaction_session = await mongoose.startSession();
 
-    // Can't delete main image, throw error
-    if (main_image === true) {
-        const illegal_update_error = new IllegalUpdateError('Cannot delete the Product Image designated as the main image ');
+    let result;
 
-        throw illegal_update_error;
-    }
+    try {
+        await mongodb_transaction_session.withTransaction(async () => {
+            console.log("Starting the process of deleting the product_image");
+
+            console.log("Getting the image_uri");
+            const image_uri = await getProductImageURI(req, mongodb_transaction_session);
+
+            console.log("Deleting the corresponding image");
+            fs.unlinkSync(image_uri);
    
-    const result = await Product.findOneAndUpdate(filter, delete_object, {new: true, runValidators: true}).lean();
+            result = await Product.findOneAndUpdate(filter, delete_object, {new: true, runValidators: true, session: mongodb_transaction_session}).lean();
 
-    console.log("result in deleteProductImage is ", result);
+            console.log("result in deleteProductImage is ", result);
+        })
+        
+    }
+
+    finally {
+        mongodb_transaction_session.endSession();
+    }
+    
     
     res.status(200).json(result);
     console.log("===END OF deleteProductImage===");
@@ -246,7 +248,6 @@ const deleteProductImage = asyncErrorHandler(async(req, res, next) => {
 
 module.exports = {
     createProductImage,
-    updateProductImageURI,
     searchProductImageById,
     deleteProductImage
 };

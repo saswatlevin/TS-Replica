9b23a2642997d8454b665c828103513a763e5fc6
Saswat Levin <saswatlevin@gmail.com>
2026-06-27 10:33:47 +0530

    Added routes for loginUser and logoutUser. Protected the the getuserbyid and searchusersbyname routes in userRoutes.js.

--- Website/src/Routes/userRoutes.js
+++ Website/src/Routes/userRoutes.js
@@ -4,13 +4,23 @@ const userControllers = require('../Controllers/userControllers');
 const requestValidator = require('../Validators/requestValidator');
 const userSchemas = require('../Schemas/userSchemas');
 const { xss } = require('express-xss-sanitizer');
+const { verifyToken } = require('../Auth/auth');
 
 router.route('/registeruser').post(requestValidator(userSchemas.userRequestSchema), xss(), userControllers.registerUser);
-router.route('/updateuser/:user_id').patch(requestValidator(userSchemas.updateUserSchema),  xss(), userControllers.updateUser);
-router.route('/updateuserpassword/:user_id').patch(requestValidator(userSchemas.updateUserPasswordSchema),  xss(), userControllers.updateUserPassword);
-router.route('/getuserbyid').get(requestValidator(userSchemas.getUserByIdSchema),  xss(), userControllers.getUserById);
-router.route('/searchusersbyname').get(requestValidator(userSchemas.searchUsersByNameSchema),  xss(), userControllers.searchUsersByName);
-router.route('/deleteuser').delete(requestValidator(userSchemas.getUserByIdSchema),  xss(), userControllers.deleteUser);
+
+router.route('/loginuser').post(requestValidator(userSchemas.loginRequestSchema), xss(), userControllers.loginUser);
+
+router.route('/logoutuser').post(userControllers.logoutUser);
+
+router.route('/updateuser/:user_id').patch(requestValidator(userSchemas.updateUserSchema), xss(), userControllers.updateUser);
+
+router.route('/updateuserpassword/:user_id').patch(requestValidator(userSchemas.updateUserPasswordSchema), xss(), userControllers.updateUserPassword);
+
+router.route('/getuserbyid').get(requestValidator(userSchemas.getUserByIdSchema), verifyToken(['admin']), xss(), userControllers.getUserById);
+
+router.route('/searchusersbyname').get(requestValidator(userSchemas.searchUsersByNameSchema), verifyToken(['admin']), xss(), userControllers.searchUsersByName);
+
+router.route('/deleteuser').delete(requestValidator(userSchemas.getUserByIdSchema), xss(), userControllers.deleteUser);
 
 
 module.exports = router;

7213cdb77b65abd4bc183ba17b7292bbb709e9a3
Saswat Levin <saswatlevin@gmail.com>
2026-06-29 10:31:53 +0530

    Added the verifyToken auth middleware to protect the registerUser, updateUser, updateUserPassword and deleteUser routes in userRoutes.js.

--- Website/src/Routes/userRoutes.js
+++ Website/src/Routes/userRoutes.js
@@ -6,21 +6,21 @@ const userSchemas = require('../Schemas/userSchemas');
 const { xss } = require('express-xss-sanitizer');
 const { verifyToken } = require('../Auth/auth');
 
-router.route('/registeruser').post(requestValidator(userSchemas.userRequestSchema), xss(), userControllers.registerUser);
+router.route('/registeruser').post(requestValidator(userSchemas.userRequestSchema),  verifyToken(['admin', 'user']), xss(), userControllers.registerUser);
 
 router.route('/loginuser').post(requestValidator(userSchemas.loginRequestSchema), xss(), userControllers.loginUser);
 
 router.route('/logoutuser').post(userControllers.logoutUser);
 
-router.route('/updateuser/:user_id').patch(requestValidator(userSchemas.updateUserSchema), xss(), userControllers.updateUser);
+router.route('/updateuser').patch(requestValidator(userSchemas.updateUserSchema),  verifyToken(['admin', 'user']), xss(), userControllers.updateUser);
 
-router.route('/updateuserpassword/:user_id').patch(requestValidator(userSchemas.updateUserPasswordSchema), xss(), userControllers.updateUserPassword);
+router.route('/updateuserpassword').patch(requestValidator(userSchemas.updateUserPasswordSchema),  verifyToken(['admin', 'user']), xss(), userControllers.updateUserPassword);
 
 router.route('/getuserbyid').get(requestValidator(userSchemas.getUserByIdSchema), verifyToken(['admin']), xss(), userControllers.getUserById);
 
 router.route('/searchusersbyname').get(requestValidator(userSchemas.searchUsersByNameSchema), verifyToken(['admin']), xss(), userControllers.searchUsersByName);
 
-router.route('/deleteuser').delete(requestValidator(userSchemas.getUserByIdSchema), xss(), userControllers.deleteUser);
+router.route('/deleteuser').delete(requestValidator(userSchemas.getUserByIdSchema),  verifyToken(['admin', 'user']), xss(), userControllers.deleteUser);
 
 
 module.exports = router;

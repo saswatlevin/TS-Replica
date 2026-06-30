bcf895092e77c4c63857ec856cf1197964b4c1ad
Saswat Levin <saswatlevin@gmail.com>
2026-07-01 01:40:18 +0530

    Protected all review routes using verifyTOken middleware in reviewRoutes.js.

--- Website/src/Routes/reviewRoutes.js
+++ Website/src/Routes/reviewRoutes.js
@@ -4,16 +4,19 @@ const reviewControllers = require('../Controllers/reviewControllers');
 const requestValidator = require('../Validators/requestValidator');
 const reviewSchemas = require('../Schemas/reviewSchemas');
 const { xss } = require('express-xss-sanitizer');
+const { verifyToken } = require('../Auth/auth');
 
-router.route('/createreview/:user_id').post(requestValidator(reviewSchemas.createReviewRequestSchema), xss(), reviewControllers.createReview);
 
-router.route('/updatereview/:user_id').patch(requestValidator(reviewSchemas.updateReviewRequestSchema), xss(), reviewControllers.updateReview);
 
-router.route('/deletereview/:user_id').delete(requestValidator(reviewSchemas.deleteReviewRequestSchema), xss(), reviewControllers.deleteReview);
+router.route('/createreview').post(requestValidator(reviewSchemas.createReviewRequestSchema), verifyToken(['admin', 'user']), xss(), reviewControllers.createReview);
 
-router.route('/searchreview/:user_id').get(requestValidator(reviewSchemas.searchReviewRequestSchema), xss(), reviewControllers.searchReview);
+router.route('/updatereview').patch(requestValidator(reviewSchemas.updateReviewRequestSchema), verifyToken(['admin', 'user']), xss(), reviewControllers.updateReview);
 
-router.route('/getreviewbyid/:user_id').get(requestValidator(reviewSchemas.getReviewByIdRequestSchema), xss(), reviewControllers.getReviewById);
+router.route('/deletereview').delete(requestValidator(reviewSchemas.deleteReviewRequestSchema), verifyToken(['admin', 'user']), xss(), reviewControllers.deleteReview);
+
+router.route('/searchreview').get(requestValidator(reviewSchemas.searchReviewRequestSchema), xss(), verifyToken(['admin', 'user']), reviewControllers.searchReview);
+
+router.route('/getreviewbyid').get(requestValidator(reviewSchemas.getReviewByIdRequestSchema), verifyToken(['admin', 'user']), xss(), reviewControllers.getReviewById);
 
 module.exports = router;
 
